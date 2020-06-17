5b6b86633bb9d9432cc2762b8926d6465a7175b3
Dusan Palider <dusan.palider@lmco.com>
2020-06-17 09:36:13 -0400

    2020.3 from TFS

---
+++ GenBOE.Database/PTM/Release Scripts/Release 2020.4/Release 2020.4.sql
@@ -0,0 +1,37 @@
+﻿EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2020.4';
+GO
+
+/*
+	## START ##
+
+	6/15/2020 [ranzalon] - BOEJ-4634 - New Revision 
+*/
+
+IF NOT EXISTS (SELECT 1 
+		FROM [dbo].[ProposalStatusLU]
+		WHERE [ProposalStatus] = 'Revised')
+BEGIN
+SET IDENTITY_INSERT [dbo].[ProposalStatusLU] ON
+
+INSERT INTO [dbo].[ProposalStatusLU]
+(ProposalStatusID, ProposalStatus)
+VALUES (8, 'Revised');
+
+SET IDENTITY_INSERT [dbo].[ProposalStatusLU] OFF
+END
+
+IF NOT EXISTS (SELECT * FROM sys.all_columns C INNER JOIN sys.tables T on C.object_id = T.object_id INNER JOIN sys.schemas S ON T.schema_id = S.schema_id WHERE S.name = 'dbo' AND 
+	T.name = 'Proposal' AND C.name = 'IsRevision')
+BEGIN 
+
+ALTER TABLE [dbo].[Proposal]
+ADD [IsRevision] bit NOT NULL
+DEFAULT 0;
+
+END
+
+/*
+	6/15/2020 [ranzalon] - BOEJ-4634 - New Revision 
+
+	## END ##
+*/
