22b4cf1c8798b8abf4fd884b78b67e477d3c5165
Dusan <dusan.palider@lmco.com>
2020-07-01 13:23:30 -0400

    Database work

---
+++ GenBOE.Database/PTM/Object Scripts/X_Data Cleanup/DeleteOrphanedAttachments.sql
@@ -0,0 +1,3 @@
+﻿-- This should never happen, but just in case, we'll delete orphaned attachments
+DELETE FROM Attachment WHERE Id NOT IN (SELECT DISTINCT AttachmentId FROM ProposalsAttachments);
+GO
