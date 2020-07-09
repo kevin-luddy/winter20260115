71bb8cc136d128d60e2c1050ea1cca0ed1b38c67
RJ <ronald.anzalone@lmco.com>
2020-07-09 17:54:31 -0400

    Link front to backend, update delete, other changes

--- GenBOE/GenTRAC.Web/Common/WebConstants.cs
+++ GenBOE/GenTRAC.Web/Common/WebConstants.cs
@@ -343,10 +343,9 @@ namespace GenTRAC.Web.Common
             /// </summary>
             public const string DELETE_PROPOSAL = "DeleteProposal";
 
-            /// <summary>
-            /// Displays Revision History Tab
+            /// Reverts the Proposal to the prior version
             /// </summary>
-            public const string DISPLAY_REVISION_HISTORY = "DisplayRevisionHistory";
+            public const string REVERT_PROPOSAL_TO_PRIOR_VERSION = "RevertProposalToPriorVersion";
 
             #endregion Proposal
 
