87edbd5b5ead54de163ac3e74ce2bec962f906df
Dusan <dusan.palider@lmco.com>
2020-07-16 12:14:13 -0400

    Cleaned up DB script issues.

--- GenBOE.Database/PTM/Release Scripts/Release 2020.4/Release 2020.4.sql
+++ GenBOE.Database/PTM/Release Scripts/Release 2020.4/Release 2020.4.sql
@@ -1,4 +1,4 @@
-﻿EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2020.4';
+﻿EXEC dbo.[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2020.4';
 GO
 
 /*
@@ -8,30 +8,30 @@ GO
 */
 
 IF NOT EXISTS (SELECT 1 
-		FROM [dbo].[ProposalStatusLU]
+		FROM dbo.[ProposalStatusLU]
 		WHERE [ProposalStatus] = 'Revised')
 BEGIN
-SET IDENTITY_INSERT [dbo].[ProposalStatusLU] ON
+SET IDENTITY_INSERT dbo.[ProposalStatusLU] ON
 
-INSERT INTO [dbo].[ProposalStatusLU]
+INSERT INTO dbo.[ProposalStatusLU]
 (ProposalStatusID, ProposalStatus)
 VALUES (8, 'Revised');
 
-SET IDENTITY_INSERT [dbo].[ProposalStatusLU] OFF
+SET IDENTITY_INSERT dbo.[ProposalStatusLU] OFF
 END
 
 IF NOT EXISTS (SELECT * FROM sys.all_columns C INNER JOIN sys.tables T on C.object_id = T.object_id INNER JOIN sys.schemas S ON T.schema_id = S.schema_id WHERE S.name = 'dbo' AND 
 	T.name = 'Proposal' AND C.name = 'RevisionOfId')
 BEGIN 
 
-ALTER TABLE [dbo].[Proposal]
+ALTER TABLE dbo.[Proposal]
 ADD [RevisionOfId] int NULL;
 
-ALTER TABLE [dbo].[Proposal]
+ALTER TABLE dbo.[Proposal]
 ADD CONSTRAINT [FK_Proposal_Proposal]
-FOREIGN KEY ([RevisionOfId]) REFERENCES [dbo].[Proposal]([ProposalID]);
+FOREIGN KEY ([RevisionOfId]) REFERENCES dbo.[Proposal]([ProposalID]);
 
-ALTER TABLE [dbo].[Proposal]
+ALTER TABLE dbo.[Proposal]
 CHECK CONSTRAINT [FK_Proposal_Proposal];
 
 END
@@ -49,16 +49,16 @@ END
 */
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ReasonCertificationNotRequiredLU]') AND type in (N'U'))
 	BEGIN
-		CREATE TABLE ReasonCertificationNotRequiredLU (
+		CREATE TABLE dbo.ReasonCertificationNotRequiredLU (
 			Id		INT				PRIMARY KEY,
 			Text	VARCHAR(50)		NOT NULL
 		);
 	END
 GO
 
-IF NOT EXISTS (SELECT 1 FROM ReasonCertificationNotRequiredLU)
+IF NOT EXISTS (SELECT 1 FROM dbo.ReasonCertificationNotRequiredLU)
 	BEGIN
-		INSERT INTO ReasonCertificationNotRequiredLU
+		INSERT INTO dbo.ReasonCertificationNotRequiredLU
 			VALUES	(1, 'Lost / Not Awarded'),
 					(2, 'Awarded Under Threshold'),
 					(3, 'Other');
@@ -73,7 +73,7 @@ GO
 
 IF NOT EXISTS (SELECT * FROM sys.all_columns C INNER JOIN sys.tables T on C.object_id = T.object_id INNER JOIN sys.schemas S ON T.schema_id = S.schema_id WHERE S.name = 'dbo' AND T.name = 'Proposal' AND C.name = 'ReasonCertificationNotRequired')
 	BEGIN
-		ALTER TABLE Proposal ADD ReasonCertificationNotRequired INT REFERENCES ReasonCertificationNotRequiredLU(Id)
+		ALTER TABLE Proposal ADD ReasonCertificationNotRequired INT REFERENCES dbo.ReasonCertificationNotRequiredLU(Id)
 	END
 GO
 /*
@@ -89,35 +89,36 @@ GO
 */
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProposalsAttachments]') AND type in (N'U'))
 BEGIN
-	CREATE TABLE ProposalsAttachments (
-		ProposalId			INT		REFERENCES Proposal(ProposalId)		NOT NULL,
-		AttachmentId		INT		REFERENCES Attachment(Id)			NOT NULL,
-		AttachmentType		INT		REFERENCES AttachmentTypeLU(Id)		NOT NULL,
+	CREATE TABLE dbo.ProposalsAttachments (
+		ProposalId			INT		REFERENCES dbo.Proposal(ProposalId)		NOT NULL,
+		AttachmentId		INT		REFERENCES dbo.Attachment(Id)			NOT NULL,
+		AttachmentType		INT		REFERENCES dbo.AttachmentTypeLU(Id)		NOT NULL,
 		IsRevisionReference	BIT		NOT NULL
 	);
-	ALTER TABLE ProposalsAttachments ADD CONSTRAINT PK_ProposalsAttachments PRIMARY KEY (ProposalId, AttachmentId);
+	ALTER TABLE dbo.ProposalsAttachments ADD CONSTRAINT PK_ProposalsAttachments PRIMARY KEY (ProposalId, AttachmentId);
 END
 GO
+
 BEGIN TRY
 	BEGIN TRAN AttachmentDataMigration
-		INSERT INTO ProposalsAttachments(ProposalId, AttachmentId, AttachmentType, IsRevisionReference)
-			SELECT ProposalId, Id, AttachmentType, 0 FROM Attachment;
+		INSERT INTO dbo.ProposalsAttachments(ProposalId, AttachmentId, AttachmentType, IsRevisionReference)
+			SELECT ProposalId, Id, AttachmentType, 0 FROM dbo.Attachment;
 
 		DROP INDEX Attachment.[IX_Attachment_ProposalID];
-		ALTER TABLE Attachment DROP [FK_Proposal_Attachment];
-		ALTER TABLE Attachment DROP [FK_AttachmentType];
-		ALTER TABLE Attachment DROP COLUMN ProposalId;
-		ALTER TABLE Attachment DROP COLUMN AttachmentType;
+		ALTER TABLE dbo.Attachment DROP [FK_Proposal_Attachment];
+		ALTER TABLE dbo.Attachment DROP [FK_AttachmentType];
+		ALTER TABLE dbo.Attachment DROP COLUMN ProposalId;
+		ALTER TABLE dbo.Attachment DROP COLUMN AttachmentType;
 	COMMIT TRAN AttachmentDataMigration;
 END TRY
 BEGIN CATCH
 	ROLLBACK TRAN AttachmentDataMigration;
 	PRINT '!!! FAILED AttachmentDataMigration !!!';
 END CATCH
+GO
+
 /*
 	7/1/2020 [Dusan]	BOEJ-4638 Post submittal attachments working with Revisioned Proposal
 
 	## END ##
 */
-
-
