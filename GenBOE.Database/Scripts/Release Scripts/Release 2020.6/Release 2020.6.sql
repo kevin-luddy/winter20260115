cbdc9e0ab5bdf0e1ee918b98af9f54e8274aaac0
RJ <ronald.anzalone@lmco.com>
2020-09-11 14:03:44 -0400

    4773 - MOQ Type Selection Tables

--- GenBOE.Database/Scripts/Release Scripts/Release 2020.6/Release 2020.6.sql
+++ GenBOE.Database/Scripts/Release Scripts/Release 2020.6/Release 2020.6.sql
@@ -28,26 +28,97 @@ END
 /*
 	## START ##
 
-	9/10/2020 [ranzalon] - BOEJ-4774 New MOQ types
+	9/10/2020 [ranzalon] - BOEJ-4773 New Tables for MOQ Types
 */
 
-IF NOT EXISTS (SELECT * FROM [dbo].[MOQTypeLU] WHERE [MOQTypeID] >= 5001 AND [MOQTypeID] <= 5009)
-BEGIN 
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MOQTypeSelection]') AND type in (N'U'))
+BEGIN
 
-INSERT INTO [dbo].[MOQTypeLU] ([MOQTypeID],[MOQType]) VALUES (5001, 'Actual Program or Task Cost Data (Historical)'),
-	(5002, 'Comparative Analysis'), 
-	(5003, 'Cost Estimating Relationships (CERs) R2'), 
-	(5004, 'Parametric Estimates'),
-	(5005, 'Analogous Relationships (ARs)'),
-	(5006, 'Statement of Work (SOW)'),
-	(5007, 'Level of Effort (LOE)'),
-	(5008, 'Subject Matter Expert (SME) Judgement'),
-	(5009, 'Non-Labor');
+CREATE TABLE [dbo].[MOQTypeSelection] (
+	[MOQTypeSelectionId] [int] NOT NULL PRIMARY KEY,
+	[TaskId] [int] NOT NULL FOREIGN KEY REFERENCES [dbo].[BOETaskElement](BOETaskElementID),
+	[MOQTypeSelection] [int] NOT NULL FOREIGN KEY REFERENCES [dbo].[MOQTypeLU](MOQTypeID),
+	[LastUpdateDate] [datetime2](7) NOT NULL,
+	[Order] [int] NOT NULL,
+	[CERName] [varchar](255) NULL,
+	[CERLocation] [varchar](255) NULL,
+	[HoursDescription] [varchar](max) NULL,
+	[SubjectMatterExpert] [varchar](max) NULL,
+	[HoursLogicAndAssumptions] [varchar](max) NULL,
+	[DurationLogicAndAssumptions] [varchar](max) NULL,
+	[EstimateTasks] [varchar](max) NULL,
+	[Rationale] [varchar](max) NULL,
+	[SkillMix] [varchar](max) NULL
+)
+
+CREATE TABLE [version].[MOQTypeSelection] (
+	[MOQTypeSelectionId] [int] NOT NULL,
+	[TaskId] [int] NOT NULL,
+	[MOQTypeSelection] [int] NOT NULL,
+	[LastUpdateDate] [datetime2](7) NOT NULL,
+	[Order] [int] NOT NULL,
+	[CERName] [varchar](255) NULL,
+	[CERLocation] [varchar](255) NULL,
+	[HoursDescription] [varchar](max) NULL,
+	[SubjectMatterExpert] [varchar](max) NULL,
+	[HoursLogicAndAssumptions] [varchar](max) NULL,
+	[DurationLogicAndAssumptions] [varchar](max) NULL,
+	[EstimateTasks] [varchar](max) NULL,
+	[Rationale] [varchar](max) NULL,
+	[SkillMix] [varchar](max) NULL,
+	[VersionId] [int] NOT NULL
+)
+
+END
+
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MOQTypeSelectionTableData]') AND type in (N'U'))
+BEGIN
+
+CREATE TABLE [dbo].[MOQTypeSelectionTableData](
+	[MOQTypeSelectionTableDataId] [int] NOT NULL PRIMARY KEY,
+	[MOQTypeSelectionId] [int] NOT NULL FOREIGN KEY REFERENCES [dbo].[MOQTypeSelection](MOQTypeSelectionId),
+	[Order] [int] NOT NULL,
+	[TableName] [varchar](255) NULL,
+	[RepositoryName] [varchar](50) NULL,
+	[QueryType] [varchar](40) NULL,
+	[DateOfReport] [datetime2](7) NULL,
+	[HistoricalProgramName] [varchar](125) NULL,
+	[ContractNumber] [varchar](255) NULL,
+	[WbsElement] [varchar](2500) NULL,
+	[PeriodOfPerformanceStartDate] [datetime2](7) NULL,
+	[PeriodOfPerformanceEndDate] [datetime2](7) NULL,
+	[TotalWbsHours] [decimal] NULL,
+	[AdditionalQueryFilters] [varchar](2500) NULL,
+	[TotalRelevantHoursAfterQueryFilters] [decimal] NULL,
+	[MiscField1] [varchar](2500) NULL,
+	[MiscField2] [varchar](2500) NULL
+)
+
+CREATE TABLE [version].[MOQTypeSelectionTableData](
+	[MOQTypeSelectionTableDataId] [int] NOT NULL,
+	[MOQTypeSelectionId] [int] NOT NULL,
+	[Order] [int] NOT NULL,
+	[TableName] [varchar](255) NULL,
+	[RepositoryName] [varchar](50) NULL,
+	[QueryType] [varchar](40) NULL,
+	[DateOfReport] [datetime2](7) NULL,
+	[HistoricalProgramName] [varchar](125) NULL,
+	[ContractNumber] [varchar](255) NULL,
+	[WbsElement] [varchar](2500) NULL,
+	[PeriodOfPerformanceStartDate] [datetime2](7) NULL,
+	[PeriodOfPerformanceEndDate] [datetime2](7) NULL,
+	[TotalWbsHours] [decimal] NULL,
+	[AdditionalQueryFilters] [varchar](2500) NULL,
+	[TotalRelevantHoursAfterQueryFilters] [decimal] NULL,
+	[MiscField1] [varchar](2500) NULL,
+	[MiscField2] [varchar](2500) NULL,
+	[VersionId] [int] NOT NULL
+)
 
 END
 
 /*
-	9/10/2020 [ranzalon] - BOEJ-4774 New MOQ types
+	9/10/2020 [ranzalon] - BOEJ-4773 New Tables for MOQ Types
 
 	## END ##
 */
