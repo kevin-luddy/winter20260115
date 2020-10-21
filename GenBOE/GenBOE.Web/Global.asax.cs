5a379abda750020e3f27e6864ae3a2e08b801644
RJ <ronald.anzalone@lmco.com>
2020-10-21 13:03:37 -0400

    Fix global asax for RMS

--- GenBOE/GenBOE.Web/Global.asax.cs
+++ GenBOE/GenBOE.Web/Global.asax.cs
@@ -480,8 +480,7 @@ namespace GenBOE
                         new ResolvedParameter(typeof(IOffloadRatesDTOLoader)),
                         new ResolvedParameter(typeof(IProjectMapDataLoader)),
                         new ResolvedParameter(typeof(RMSZoneTravelRatesFeesDataLoader)),
-                        new ResolvedParameter(typeof(IRteTemplateDataLoader)),
-                        new ResolvedParameter(typeof(IMoqTypeDataLoader))));
+                        new ResolvedParameter(typeof(IRteTemplateDataLoader))));
                     GenBOEUnityContainer.Container.RegisterType(typeof(IBOEOtherDirectCostControllerLogic), typeof(BOEOtherDirectCostControllerLogicMST), GetLifetimeManager(), new InjectionConstructor());
                     GenBOEUnityContainer.Container.RegisterType(typeof(IBOEMaterialControllerLogic), typeof(BOEMaterialControllerLogicMST), GetLifetimeManager(), new InjectionConstructor());
                     GenBOEUnityContainer.Container.RegisterType(typeof(IGenBOEControllerLogic), typeof(GenBOEControllerLogicMST), GetLifetimeManager(), new InjectionMember[] { });
