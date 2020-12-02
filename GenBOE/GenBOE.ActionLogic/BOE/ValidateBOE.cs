c2d59d00460ee42bee1fd0db76906e03b0ec756b
RJ <ronald.anzalone@lmco.com>
2020-12-01 19:00:09 -0500

    4957 - Remove the Total WBS/WBS Element Hours for SSC

--- GenBOE/GenBOE.ActionLogic/BOE/ValidateBOE.cs
+++ GenBOE/GenBOE.ActionLogic/BOE/ValidateBOE.cs
@@ -694,6 +694,7 @@ namespace GenBOE.ActionLogic.WBS.BOE
                                 else
                                 {
                                     ValidateRequiredField(moqType.SelectedMOQType, row.ContractNumber, labels.ContractNumber, errorMessages);
+                                    if (row.TotalWbsHours <= 0) { errorMessages.Add($"{moqType.SelectedMOQType.GetDescription()}: {labels.TotalWbsHours} must be a number greater than 0."); }
                                 }
 
                                 if (row.DateOfReport.Year == 1) { errorMessages.Add($"{moqType.SelectedMOQType.GetDescription()}: {labels.DateOfReport} is required."); }
@@ -708,8 +709,7 @@ namespace GenBOE.ActionLogic.WBS.BOE
                                 if (row.PoPEnd > DateTime.Now.Date) { errorMessages.Add($"{moqType.SelectedMOQType.GetDescription()}: {labels.PoPEnd} must be on or before today's date."); }
 
                                 if (row.PoPEnd < row.PoPStart) { errorMessages.Add($"{moqType.SelectedMOQType.GetDescription()}: {labels.PoPStart} must be on or before {labels.PoPEnd}."); }
-                                if (row.TotalWbsHours <= 0) { errorMessages.Add($"{moqType.SelectedMOQType.GetDescription()}: {labels.TotalWbsHours} must be a number greater than 0."); }
-
+                                
                                 ValidateRequiredField(moqType.SelectedMOQType, row.AdditionalQueryFilters, labels.AdditionalQueryFilters, errorMessages);
 
                                 if (row.TotalRelevantHours <= 0) { errorMessages.Add($"{moqType.SelectedMOQType.GetDescription()}: {labels.TotalRelevantHours} must be a number greater than 0."); }
