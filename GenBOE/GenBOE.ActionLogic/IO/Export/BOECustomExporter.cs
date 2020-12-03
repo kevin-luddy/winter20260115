239bc7121f0188e09ad9eb67bf733cbfd5e86e0b
Dusan <dusan.palider@lmco.com>
2020-12-03 15:58:47 -0500

    BOEJ-4971: Update a label text;

--- GenBOE/GenBOE.ActionLogic/IO/Export/BOECustomExporter.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/BOECustomExporter.cs
@@ -2419,7 +2419,7 @@ namespace GenBOE.ActionLogic.IO.Export
                     if (wsHasMoqRteTemplate)
                     {
                         // Update label
-                        laborTaskHeaderDataValueMappings.Add(BOEExporterConstants.FieldName_MethodOfQuotingLabel, "Additional MOQ Types Information: ");
+                        laborTaskHeaderDataValueMappings.Add(BOEExporterConstants.FieldName_MethodOfQuotingLabel, "Additional MOQ Rationale: ");
 
                         // populate RTE MOQ text
                         if (WordUtilities.GetTaggedChildElement(containerElement, BOEExporterConstants.FieldName_MethodOfQuoting) != null)
