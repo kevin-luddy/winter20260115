350b5ac52f59d4dadcb39eeadf221363b2402ec7
Dusan <dusan.palider@lmco.com>
2021-04-13 17:01:40 -0400

    An additional fix due to an additional item being found.

--- GenBOE.Database/PTM/Release Scripts/Release 2021.6/Release 2021.6.sql
+++ GenBOE.Database/PTM/Release Scripts/Release 2021.6/Release 2021.6.sql
@@ -29,6 +29,12 @@ GO
 
 EXEC dbo.CopyCannedResponsesPAR 14;
 GO
+
+UPDATE new
+	SET YesOnly = old.YesOnly
+	FROM PARChecklistContent new INNER JOIN PARChecklistContent old ON old.QuestionNumber = new.QuestionNumber
+	WHERE new.ProposalAdequacyReviewID = 14 AND old.ProposalAdequacyReviewID = 13 - 1;
+GO
 /*
 	4/13/2021 [Dusan] - BOEJ-5211: Canned Responses not a part of the new revision;
 
