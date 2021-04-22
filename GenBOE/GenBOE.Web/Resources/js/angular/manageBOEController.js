d4ca57c9b81006aee2c0b4cce51a403e9117d5cc
RJ <ronald.anzalone@lmco.com>
2021-04-22 18:27:34 -0400

    5218 - updating sorting of boes list in Bulk Assign

--- GenBOE/GenBOE.Web/Resources/js/angular/manageBOEController.js
+++ GenBOE/GenBOE.Web/Resources/js/angular/manageBOEController.js
@@ -484,8 +484,19 @@
             }
         }
 
+        $scope.updateBulkAssignData();
+
         $scope.SaveFilterToCookies();
     };
+
+    $scope.updateBulkAssignData = function () {
+        $scope.bulkAssignBoes = [];
+        var sortedBoes = $filter('orderBy')($scope.isBulkAssign ? $scope.bulkAssignData : $scope.data, $scope.predicate, $scope.reverse);
+
+        sortedBoes.forEach(function (boe) {
+            $scope.bulkAssignBoes.push({ id: boe.BoeID, label: boe.WbsDisplayName + " | " + boe.ClinDisplayName });
+        });
+    }
 
     // returns true when the column being sorted matches the first index
     $scope.boldSort = function (sortColumn) {
@@ -1036,10 +1047,6 @@
 
             $scope.data = response.data.BoeResults; 
 
-            $scope.data.forEach(function (boe) {
-                $scope.bulkAssignBoes.push({ id: boe.BoeID, label: boe.WbsDisplayName + " | " + boe.ClinDisplayName });
-            });
-
             $scope.gridModel.PotentialAuthors.forEach(function (user) {
                 $scope.bulkAssignAuthors.push({ id: user.Value, label: user.Text });
             });
@@ -1053,6 +1060,8 @@
             });
 
             $scope.LoadFilterFromCookies();
+
+            $scope.updateBulkAssignData();
 
             $scope.isLoading = false;
             firstLoad = false;
