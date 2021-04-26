472a806adb8f4565f995e373b75fe49cad77313a
RJ <ronald.anzalone@lmco.com>
2021-04-26 17:48:56 -0400

    5218 - updateBulkAssignData => sortBulkAssignData

--- GenBOE/GenBOE.Web/Resources/js/angular/manageBOEController.js
+++ GenBOE/GenBOE.Web/Resources/js/angular/manageBOEController.js
@@ -484,12 +484,12 @@
             }
         }
 
-        $scope.updateBulkAssignData();
+        $scope.sortBulkAssignData();
 
         $scope.SaveFilterToCookies();
     };
 
-    $scope.updateBulkAssignData = function () {
+    $scope.sortBulkAssignData = function () {
         $scope.bulkAssignBoes = [];
         var sortedBoes = $filter('orderBy')($scope.isBulkAssign ? $scope.bulkAssignData : $scope.data, $scope.predicate, $scope.reverse);
 
@@ -1061,7 +1061,7 @@
 
             $scope.LoadFilterFromCookies();
 
-            $scope.updateBulkAssignData();
+            $scope.sortBulkAssignData();
 
             $scope.isLoading = false;
             firstLoad = false;
