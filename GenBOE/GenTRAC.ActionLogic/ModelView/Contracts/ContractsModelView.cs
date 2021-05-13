e952e7fd5e7d508604aec08c93c435e6ffeb63ea
Dusan <dusan.palider@lmco.com>
2021-05-13 09:44:25 -0400

    BOEJ-5237: First UI work.

---
+++ GenBOE/GenTRAC.ActionLogic/ModelView/Contracts/ContractsModelView.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2020 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenTRAC.ActionLogic.ModelView
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using IES.Common;
+
+    public class ContractsModelView
+    {
+        [Display(Name= "Previously Submitted ROM")]
+        public string PreviouslySubmittedROM { get; set; }
+
+        private DateTime? previousROMDate { get; set; }
+
+        [Display(Name = "Previously Submitted ROM Date")]
+        public DateTime? PreviousROMDate { get => this.previousROMDate?.Date; set => this.previousROMDate = value?.Normalize(DateTimePrecision.Day); }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1044:PropertiesShouldNotBeWriteOnly")]
+        public decimal? PreviousROMValueDecimal { private get; set; }
+
+        [Display(Name = "Previously Submitted ROM Value")]
+        public string PreviousROMValue => this.PreviousROMValueDecimal?.ToString("C");
+
+        private DateTime? customerSubmittalDate { get; set; }
+
+        [Display(Name = "Customer Submittal Date")]
+        public DateTime? CustomerSubmittalDate { get => this.customerSubmittalDate?.Date; set => this.customerSubmittalDate = value?.Normalize(DateTimePrecision.Day); }
+
+        [Display(Name = "Contracts Correspondence Log Number")]
+        public string ContractsCorrespondenceLogNumber { get; set; }
+    }
+}
--- GenBOE/GenTRAC.Web/Controllers/ContractsController.cs
+++ GenBOE/GenTRAC.Web/Controllers/ContractsController.cs
@@ -6,8 +6,10 @@
 
 namespace GenTRAC.Web.Controllers
 {
+    using System;
     using System.Web.Mvc;
     using GenTRAC.ActionLogic;
+    using GenTRAC.ActionLogic.ModelView;
     using GenTRAC.Web.Common;
     using IES.Common;
 
@@ -44,7 +46,9 @@ namespace GenTRAC.Web.Controllers
             this.ViewBag.ProposalId = proposalId.ToString();
             this.ViewBag.ReadOnly = false; // todo.. fix me
 
-            return this.View(WebConstants.View.CONTRACTS_INDEX, null);
+            ContractsModelView model = new ContractsModelView() { PreviousROMValueDecimal = 1000, PreviouslySubmittedROM = "rom", ContractsCorrespondenceLogNumber = "log #", PreviousROMDate = DateTime.Now.AddDays(-100), CustomerSubmittalDate = DateTime.Now.AddDays(-1) };
+
+            return this.View(WebConstants.View.CONTRACTS_INDEX, model);
         }
     }
 }
