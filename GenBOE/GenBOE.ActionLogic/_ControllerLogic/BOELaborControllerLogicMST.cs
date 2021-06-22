8598197d347c57dda5640cce5cf9f441ad0f2cee
RJ <ronald.anzalone@lmco.com>
2021-06-22 15:00:54 -0400

    5260 - MOQ Table Export

--- GenBOE/GenBOE.ActionLogic/_ControllerLogic/BOELaborControllerLogicMST.cs
+++ GenBOE/GenBOE.ActionLogic/_ControllerLogic/BOELaborControllerLogicMST.cs
@@ -10,21 +10,22 @@ namespace GenBOE.ActionLogic
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
-    using GenBOE.ActionLogic.ModelView;
     using GenBOE.ActionLogic.BLL;
     using GenBOE.ActionLogic.BOETransitions;
     using GenBOE.ActionLogic.Common;
     using GenBOE.ActionLogic.Common.Calculations;
     using GenBOE.ActionLogic.ControllerLogic;
+    using GenBOE.ActionLogic.IO.Export;
+    using GenBOE.ActionLogic.ModelView;
     using GenBOE.ActionLogic.ModelView.BOE;
     using GenBOE.ActionLogic.Validation;
+    using GenBOE.ActionLogic.WBS.BOE;
     using GenBOE.DataBridge.Common;
     using GenBOE.DataBridge.DTO;
     using GenBOE.Dtos;
     using GenBOE.Objects;
     using IES.Common;
     using IES.Common.classes;
-    using GenBOE.ActionLogic.WBS.BOE;
 
     public class BOELaborControllerLogicMST : BOELaborControllerLogic
     {
@@ -75,7 +76,8 @@ namespace GenBOE.ActionLogic
             ICommonDataMapper commonDataMapper,
             IRteTemplateDataLoader rteTemplateDataLoader,
             IMoqTypeDataLoader moqTypeDataLoader,
-            IValidateBOE validateBOE
+            IValidateBOE validateBOE,
+            IMoqTableExporter moqTableExporter
             )
             : base(inBoeTaskElementRecalc,
                 inBoeStateMachine,
@@ -96,7 +98,8 @@ namespace GenBOE.ActionLogic
                 commonDataMapper,
                 rteTemplateDataLoader,
                 moqTypeDataLoader,
-                validateBOE)
+                validateBOE,
+                moqTableExporter)
         {
             this._mstMetricsLoader = inMSTMetricLoader;
             this.rteTemplateDataLoader = rteTemplateDataLoader;
