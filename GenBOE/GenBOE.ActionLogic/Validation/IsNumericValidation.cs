727283cd1f8120f4ae8173b0016a6ee28eabb0a3
RJ <ronald.anzalone@lmco.com>
2021-08-25 17:57:07 -0400

    IES-135 - update copyright to 2021

--- GenBOE/GenBOE.ActionLogic/Validation/IsNumericValidation.cs
+++ GenBOE/GenBOE.ActionLogic/Validation/IsNumericValidation.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="IsNumericValidation.cs" company="IS&GS;">
-//     Copyright (c) 2011 - 2020 Lockheed Martin Corporation
+//     Copyright (c) 2011 - 2021 Lockheed Martin Corporation
 // </copyright>
 // -----------------------------------------------------------------------
 
