6d0db1a4cbb99501cb72ce9d17836af7a8af8287
Thomas Asuncion <thomas.asuncion@lmco.com>
2021-11-04 11:30:33 -0500

    Update WBSElement Field Length

---
+++ GenBOE.Database/Scripts/Release Scripts/Release 2021.1.2/Release 2021.1.2.sql
@@ -0,0 +1,21 @@
+﻿EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2021.1.2';
+GO
+
+/*
+    ## START ##
+
+    11/3/2021 [Asuncion] - IES-444 MOQ Type Table - WBS/WBS Element Character Limit Increase
+*/
+
+IF COL_LENGTH ('dbo.MOQTypeSelectionTableData', 'WbsElement') IS NOT NULL
+BEGIN
+    ALTER TABLE dbo.MOQTypeSelectionTableData
+    ALTER COLUMN WbsElement varchar(5000) NOT NULL
+END
+GO
+
+/*
+    11/3/2021 [Asuncion] - IES-444 MOQ Type Table - WBS/WBS Element Character Limit Increase
+
+    ## END ##
+*/
