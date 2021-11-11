f75eb37b20337b513a8a8384d98e196e8083ba0f
Alex Remaker <alex.remaker@lmco.com>
2021-11-11 10:03:34 -0500

    addressed pull request feedback.  implemented new logic retrieve file in ExportAllBOEs endpoint

--- GenBOE/GenBOE.Web/Controllers/BoeDataAPIController.cs
+++ GenBOE/GenBOE.Web/Controllers/BoeDataAPIController.cs
@@ -6,12 +6,14 @@
 
 namespace GenBOE.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
 	using System.Collections.ObjectModel;
 	using System.IO;
 	using System.Linq;
 	using System.Net;
+	using System.Net.Http;
+	using System.Net.Http.Headers;
 	using System.Security.Principal;
 	using System.Web;
 	using System.Web.Http;
@@ -23,157 +25,183 @@ namespace GenBOE.Web.Controllers
 	using GenBOE.Dtos;
 	using GenBOE.Objects;
 	using GenBOE.Web.ModelView;
-    using IES.Common;
+	using IES.Common;
 
 	/// <summary>
 	/// BOE Data Controller, original intent is for it to be used by ACV to pull data in, but realistically, it is serving up BOE data, hence the name.
 	/// </summary>
 	[AllowAnonymous]
-    public class BoeDataAPIController : BoeDataBaseAPIController
-    {
-        #region Properties & Ctor
+	public class BoeDataAPIController : BoeDataBaseAPIController
+	{
+		#region Properties & Ctor
 
-        /// <summary>
-        /// Proposal Loader
-        /// </summary>
-        private IWorkspaceDTODataLoader loader;
+		/// <summary>
+		/// Proposal Loader
+		/// </summary>
+		private IWorkspaceDTODataLoader loader;
 
-        /// <summary>
-        /// Token Handling
-        /// </summary>
-        private TokenHandling tokenHandler;
+		/// <summary>
+		/// Token Handling
+		/// </summary>
+		private TokenHandling tokenHandler;
 
-        /// <summary>
-        /// Reports controller
-        /// </summary>
-        private IReportsControllerLogic reportsControllerLogic;
+		/// <summary>
+		/// Reports controller
+		/// </summary>
+		private IReportsControllerLogic reportsControllerLogic;
 
-        private IBOEExporter boeExporter;
+		/// <summary>
+		/// BOE Exporter
+		/// </summary>
+		private IBOEExporter boeExporter;
 
-        private IBOECustomExporter boeCustomExporter;
+		/// <summary>
+		/// BOE custom exporter
+		/// </summary>
+		private IBOECustomExporter boeCustomExporter;
 
-        private IWorkspaceExportFormatDTODataLoader workspaceExportFormatDTOLoader;
+		/// <summary>
+		/// Workspace Export fromat loader
+		/// </summary>
+		private IWorkspaceExportFormatDTODataLoader workspaceExportFormatDTOLoader;
 
-        /// <summary>
-        /// Logger
-        /// </summary>
-        private Logger logger = new Logger("BoeDataAPIController");
+		/// <summary>
+		/// Logger
+		/// </summary>
+		private Logger logger = new Logger("BoeDataAPIController");
 
-        /// <summary>
-        /// Ctor
-        /// </summary>
-        public BoeDataAPIController(IWorkspaceDTODataLoader loader, TokenHandling tokenHandler, IReportsControllerLogic reportsControllerLogic, ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader, IBOEExporter boeExporter, IBOECustomExporter boeCustomExporter, IWorkspaceExportFormatDTODataLoader workspaceExportFormatDTOLoader) 
-            : base(securityAccess, factory, userLoader, permissionsLoader)
-        {
-            this.loader = loader;
-            this.tokenHandler = tokenHandler;
-            this.reportsControllerLogic = reportsControllerLogic;
-            this.boeExporter = boeExporter;
-            this.boeCustomExporter = boeCustomExporter;
-            this.workspaceExportFormatDTOLoader = workspaceExportFormatDTOLoader;
-        }
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="loader">Workspace loader</param>
+		/// <param name="tokenHandler">Token handler</param>
+		/// <param name="reportsControllerLogic">Reports controller logic</param>
+		/// <param name="securityAccess">Security Access</param>
+		/// <param name="factory">Full object factory</param>
+		/// <param name="userLoader">User loader</param>
+		/// <param name="permissionsLoader">Permission loader</param>
+		/// <param name="boeExporter">BOE exporter</param>
+		/// <param name="boeCustomExporter">BOE custom exporter</param>
+		/// <param name="workspaceExportFormatDTOLoader">Workspace export format loader</param>
+		public BoeDataAPIController(IWorkspaceDTODataLoader loader, TokenHandling tokenHandler, IReportsControllerLogic reportsControllerLogic, ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader, IBOEExporter boeExporter, IBOECustomExporter boeCustomExporter, IWorkspaceExportFormatDTODataLoader workspaceExportFormatDTOLoader) 
+			: base(securityAccess, factory, userLoader, permissionsLoader)
+		{
+			this.loader = loader;
+			this.tokenHandler = tokenHandler;
+			this.reportsControllerLogic = reportsControllerLogic;
+			this.boeExporter = boeExporter;
+			this.boeCustomExporter = boeCustomExporter;
+			this.workspaceExportFormatDTOLoader = workspaceExportFormatDTOLoader;
+		}
+		#endregion
 
-        #endregion
+		/// <summary>
+		/// Get Proposal data for ACV. Limits the number of records returned to 100.
+		/// </summary>
+		/// <param name="ptmTrackingNumber">PTM Tracking Number</param>
+		/// <returns>Proposal Data</returns>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public ICollection<AcvWorkspaceData> GetWorkspaceDataForProposal(string ptmTrackingNumber)
+		{
+			List<AcvWorkspaceData> result;
 
-        /// <summary>
-        /// Get Proposal data for ACV. Limits the number of records returned to 100.
-        /// </summary>
-        /// <param name="ptmTrackingNumber">PTM Tracking Number</param>
-        /// <returns>Proposal Data</returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        [HttpGet]
-        public ICollection<AcvWorkspaceData> GetWorkspaceDataForProposal(string ptmTrackingNumber)
-        {
-            List<AcvWorkspaceData> result;
-
-            try
-            {
-                tokenHandler.AuthenticateUserFromAuthorizationToken();
-
-                ICollection<(int Id, string shortName, string longName)> data = this.loader.GetWorkspaceDataForProposal(ptmTrackingNumber);
-                result = data.Select(x => new AcvWorkspaceData() { Id = x.Id, ShortName = x.shortName, LongName = x.longName }).ToList();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-                result = null;
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Export all BOEs given workspace 
-        /// </summary>
-        /// <param name="workspaceId">Id of workspace</param>
-        /// <returns></returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        [HttpGet]
-        public IHttpActionResult ExportAllBOEs(string workspaceId)
-        {
-            try
-            {
+			try
+			{
 				tokenHandler.AuthenticateUserFromAuthorizationToken();
 
-				HttpResponse response = HttpContext.Current.Response;
-                FullWorkspace workspace = this.Factory.CreateFullWorkspace(workspaceId);
+				ICollection<(int Id, string shortName, string longName)> data = this.loader.GetWorkspaceDataForProposal(ptmTrackingNumber);
+				result = data.Select(x => new AcvWorkspaceData() { Id = x.Id, ShortName = x.shortName, LongName = x.longName }).ToList();
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+				result = null;
+			}
 
-				var permission = this.CheckPermission(SecurityPage.Reports, workspace);
+			return result;
+		}
+
+		/// <summary>
+		/// Export all BOEs given workspace 
+		/// </summary>
+		/// <param name="workspaceId">Id of workspace</param>
+		/// <returns></returns>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		[HttpGet]
+		public HttpResponseMessage ExportAllBOEs(string workspaceId)
+		{
+			try
+			{
+
+				tokenHandler.AuthenticateUserFromAuthorizationToken();
+
+				FullWorkspace workspace = this.Factory.CreateFullWorkspace(workspaceId);
+
+				SecurityAuthorization permission = this.CheckPermission(SecurityPage.Reports, workspace);
 
 				if (permission < SecurityAuthorization.Read)
 				{
-					return Unauthorized();
+					new HttpResponseMessage(HttpStatusCode.Unauthorized);
 				}
 
-				this.reportsControllerLogic.PrepareAllBOEsReport(workspace, this.PermissionsLoader.GetBOEPotentialPermissionsForWorkspace(workspace.Id).Any(p => p.Role == Role.SubcontractorAuthor && p.ETIUserId == workspace.CurrentActiveUser.UserID), null, null, null, out bool isCustomExport,
-					out WorkspaceExportFormatDTO wsExportFormatDTO, out BOEExportInputs exportInputs, out ICollection<BOEExportModelView> boeExportModelViews, out List<BOESummaryGridModelView> boeSummaryGridModelViews, false);
+				HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
 
-                string fileName = string.Format("genBOECustomExport-{0}.docx", workspace.WorkspaceName).Replace(",", string.Empty);
+				string fileName = string.Format("genBOE-Export-{0}.docx", workspace.WorkspaceName).Replace(",", string.Empty);
 
-                response.ContentType = BOEExporterConstants.ContentType_DOCX;
-                response.Clear();
-                response.BufferOutput = true;
-                response.AppendHeader(BOEExporterConstants.CONTENT_HEADER_NAME, string.Format(BOEExporterConstants.CONTENT_HEADER_FORMAT_STRING, fileName));
+				MemoryStream stream = new MemoryStream();
 
-                if (isCustomExport)
+				this.reportsControllerLogic.PrepareAllBOEsReport(workspace, this.PermissionsLoader.GetBOEPotentialPermissionsForWorkspace(workspace.Id).Any(p => p.Role == Role.SubcontractorAuthor && p.ETIUserId == workspace.CurrentActiveUser.UserID), null, null, null, out bool isCustomExport, out WorkspaceExportFormatDTO wsExportFormatDTO, out BOEExportInputs exportInputs, out ICollection<BOEExportModelView> boeExportModelViews, out List<BOESummaryGridModelView> boeSummaryGridModelViews, false);
+
+				if (isCustomExport)
 				{
-                    WorkspaceExportFormatDTO exportFormat = wsExportFormatDTO.ExportFormat.ParentTemplateId < 9001 || wsExportFormatDTO.ExportFormat.ParentTemplateId > 10000 || wsExportFormatDTO.ExportFormat.ParentTemplateId == null ? this.workspaceExportFormatDTOLoader.GetById((int)ExcelReportTemplateType.MASTER) : wsExportFormatDTO;
-                    boeCustomExporter.ExportBOEToWordFileStream(exportInputs, boeExportModelViews, boeSummaryGridModelViews, workspace, null, response.OutputStream, exportFormat);
-                }
+					WorkspaceExportFormatDTO exportFormat = wsExportFormatDTO.ExportFormat.ParentTemplateId < 9001 || wsExportFormatDTO.ExportFormat.ParentTemplateId > 10000 || wsExportFormatDTO.ExportFormat.ParentTemplateId == null ? this.workspaceExportFormatDTOLoader.GetById((int)ExcelReportTemplateType.MASTER) : wsExportFormatDTO;
+					boeCustomExporter.ExportBOEToWordFileStream(exportInputs, boeExportModelViews, boeSummaryGridModelViews, workspace, null, stream, exportFormat);
+				}
 				else
 				{
-                    boeExporter.ExportBOEToWordFileStream(exportInputs, boeExportModelViews, boeSummaryGridModelViews, workspace, fileName, response.OutputStream, wsExportFormatDTO.ExportFormat.TemplateType);
-                }
+					boeExporter.ExportBOEToWordFileStream(exportInputs, boeExportModelViews, boeSummaryGridModelViews, workspace, fileName, stream, wsExportFormatDTO.ExportFormat.TemplateType);
+				}
 
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-                return InternalServerError();
-            }
-        }
+				stream.Position = 0; //We need to set this to return the file
 
-        /// <summary>
-        /// Is Service Alive?
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        [HttpGet]
-        public bool IsAlive()
-        {
-            bool result;
+				response.Content = new StreamContent(stream);
+				response.Content.Headers.ContentType = new MediaTypeHeaderValue(BOEExporterConstants.ContentType_DOCX);
+				response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+				{
+					FileName = fileName
+				};
 
-            try
-            {
-                // ToDo: add a DB grab, just to see if the DB is working.. To make the check more meaningful
-                result = true;
-            }
-            catch
-            {
-                result = false;
-            }
+				return response;
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+				return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+			}
+		}
 
-            return result;
-        }
-    }
+		/// <summary>
+		/// Is Service Alive?
+		/// </summary>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public bool IsAlive()
+		{
+			bool result;
+
+			try
+			{
+				// ToDo: add a DB grab, just to see if the DB is working.. To make the check more meaningful
+				result = true;
+			}
+			catch
+			{
+				result = false;
+			}
+
+			return result;
+		}
+	}
 }
--- GenBOE/GenBOE.Web/Controllers/BoeDataBaseAPIController.cs
+++ GenBOE/GenBOE.Web/Controllers/BoeDataBaseAPIController.cs
@@ -1,29 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using GenBOE.DataBridge.Common;
-using GenBOE.DataBridge.Common.Interfaces;
-using GenBOE.DataBridge.DTO;
-using GenBOE.Dtos;
-using GenBOE.Objects;
-using IES.Common;
-using IES.Common.Exceptions;
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2021 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace GenBOE.Web.Controllers
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Web;
+	using System.Web.Http;
+	using GenBOE.DataBridge.Common;
+	using GenBOE.DataBridge.Common.Interfaces;
+	using GenBOE.DataBridge.DTO;
+	using GenBOE.Dtos;
+	using GenBOE.Objects;
+	using IES.Common;
+	using IES.Common.Exceptions;
+
 	/// <summary>
 	/// Base BOE Api data controller
 	/// </summary>
 	public class BoeDataBaseAPIController : ApiController
 	{
-        protected ISecurityAccess SecurityAccess { get; set; }
-        protected IFullObjectFactory Factory { get; set; }
-        protected IUserDTODataLoader UserLoader { get; set; }
-        protected IPermissionsDTODataLoader PermissionsLoader { get; set; }
+		protected ISecurityAccess SecurityAccess { get; set; }
+		protected IFullObjectFactory Factory { get; set; }
+		protected IUserDTODataLoader UserLoader { get; set; }
+		protected IPermissionsDTODataLoader PermissionsLoader { get; set; }
 
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="securityAccess">Security access</param>
+		/// <param name="factory">Full object factory</param>
+		/// <param name="userLoader">User loader</param>
+		/// <param name="permissionsLoader">Permission loader</param>
 		public BoeDataBaseAPIController(ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader)
 		{
 			this.Factory = factory;
@@ -32,40 +46,45 @@ namespace GenBOE.Web.Controllers
 			this.SecurityAccess = securityAccess;
 		}
 
+		/// <summary>
+		/// Checks permission for page (Some logic pulled from GenBOEController.cs)
+		/// </summary>
+		/// <param name="page">Security Page</param>
+		/// <param name="workspace">Workspace</param>
+		/// <returns></returns>
+		protected SecurityAuthorization CheckPermission(SecurityPage page, WorkspaceDTO workspace)
+		{
+			Dictionary<SecurityPage, SecurityAuthorization> securityDictionary = new Dictionary<SecurityPage, SecurityAuthorization>();
+			int? wsId = workspace == null ? null : (int?)workspace.Id;
 
-        protected SecurityAuthorization CheckPermission(SecurityPage page, WorkspaceDTO workspace)
-        {
-            Dictionary<SecurityPage, SecurityAuthorization> securityDictionary = new Dictionary<SecurityPage, SecurityAuthorization>();
-            int? wsId = workspace == null ? null : (int?)workspace.Id;
+			UserDTO user = this.UserLoader.GetUserForActiveUser();
+			string overrideNonUsString = ConfigurationUtilities.GetAppSetting("OverrideSubNonUs");
+			bool overrideNonUs = string.IsNullOrEmpty(overrideNonUsString) ? false : overrideNonUsString.ToLower() == "true";
+			bool? isUsPerson = overrideNonUs ? true : user.IsUsPerson;
 
-            UserDTO user = this.UserLoader.GetUserForActiveUser();
-            string overrideNonUsString = ConfigurationUtilities.GetAppSetting("OverrideSubNonUs");
-            bool overrideNonUs = string.IsNullOrEmpty(overrideNonUsString) ? false : overrideNonUsString.ToLower() == "true";
-            bool? isUsPerson = overrideNonUs ? true : user.IsUsPerson;
+			if (isUsPerson == null)
+			{
+				throw new ValidationException("IsUsPerson cannot be null");
+			}
 
-            if (isUsPerson == null)
-            {
-                throw new ValidationException("IsUsPerson cannot be null");
-            }
+			IReadOnlyCollection<SecurityPermissionsResponse> rolesForUser = this.Factory.GetPermissionsForUser(user.NTID);
 
-            IReadOnlyCollection<SecurityPermissionsResponse> rolesForUser = this.Factory.GetPermissionsForUser(user.NTID);
+			SecurityAuthorization authorizationForUser;
 
-            SecurityAuthorization authorizationForUser;
+			if ((bool)isUsPerson)
+			{
+				authorizationForUser = SecurityAccess.IsAuthorized(
+					new SecurityPermissionsRequested { PageToCheck = page, WorkspaceId = wsId }, workspace, rolesForUser);
+			}
+			else
+			{
+				throw new UnauthorizedAccessException("Access is denied for non-US users.");
+			}
 
-            if ((bool)isUsPerson)
-            {
-                authorizationForUser = SecurityAccess.IsAuthorized(
-                    new SecurityPermissionsRequested { PageToCheck = page, WorkspaceId = wsId }, workspace, rolesForUser);
-            }
-            else
-            {
-                throw new UnauthorizedAccessException("Access is denied for non-US users.");
-            }
+			securityDictionary.Add(page, authorizationForUser);
 
-            securityDictionary.Add(page, authorizationForUser);
+			return securityDictionary.Values.First();
+		}
 
-            return securityDictionary.Values.First();
-        }
-
-    }
+	}
 }
