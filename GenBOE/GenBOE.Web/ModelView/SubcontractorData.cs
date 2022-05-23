053d87a873f20bb6c58e3257d8c9925f56a07b4c
Timothy Wilson <timothy.wilson@lmco.com>
2022-05-23 17:59:05 -0400

    Get subcontractor data for workspace

---
+++ GenBOE/GenBOE.Web/ModelView/SubcontractorData.cs
@@ -0,0 +1,18 @@
+﻿namespace GenBOE.Web.ModelView
+{
+	/// <summary>
+	/// Subcontractor Data inside a Workspace
+	/// </summary>
+	public class SubcontractorData
+	{
+		/// <summary>
+		/// The Subcontractor Name
+		/// </summary>
+		public string Name { get; set; }
+		
+		/// <summary>
+		/// The Total Cost for a Subcontractor
+		/// </summary>
+		public decimal Cost { get; set; }
+	}
+}
