38541b70aa60e365c51b993118de6098259c81e2
RJ <ronald.anzalone@lmco.com>
2022-06-09 08:59:07 -0400

    IES-1343 - spaces to tabs in api classes

--- GenBOE/IES.Common/Utilities/WebApiConfig.cs
+++ GenBOE/IES.Common/Utilities/WebApiConfig.cs
@@ -16,53 +16,53 @@ namespace IES.Common
 	using Elmah.Contrib.WebApi;
 
 	public static class WebApiConfig
-    {
-        /// <summary>
-        /// Registers the specified configuration.
-        /// </summary>
-        /// <param name="config">The configuration.</param>
-        /// <exception cref="System.ArgumentNullException">config</exception>
-        public static void Register(HttpConfiguration config)
-        {
-            _ = config ?? throw new ArgumentNullException(nameof(config));
+	{
+		/// <summary>
+		/// Registers the specified configuration.
+		/// </summary>
+		/// <param name="config">The configuration.</param>
+		/// <exception cref="System.ArgumentNullException">config</exception>
+		public static void Register(HttpConfiguration config)
+		{
+			_ = config ?? throw new ArgumentNullException(nameof(config));
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
 
-            JsonMediaTypeFormatter formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
-            formatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+			JsonMediaTypeFormatter formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+			formatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
 
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
-            // enable elmah
-            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
+			// enable elmah
+			config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
 
-            config.Filters.Add(new UnhandledExceptionFilterAttribute());
-        }
-    }
+			config.Filters.Add(new UnhandledExceptionFilterAttribute());
+		}
+	}
 
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
-    public sealed class UnhandledExceptionFilterAttribute : ExceptionFilterAttribute
-    {
-        /// <summary>
-        /// Raises the exception event.
-        /// </summary>
-        /// <param name="actionExecutedContext">The context for the action.</param>
-        /// <exception cref="System.ArgumentNullException">context</exception>
-        public override void OnException(HttpActionExecutedContext actionExecutedContext)
-        {
-            if (ReferenceEquals(actionExecutedContext, null))
-            {
-                throw new ArgumentNullException(nameof(actionExecutedContext));
-            }
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+	public sealed class UnhandledExceptionFilterAttribute : ExceptionFilterAttribute
+	{
+		/// <summary>
+		/// Raises the exception event.
+		/// </summary>
+		/// <param name="actionExecutedContext">The context for the action.</param>
+		/// <exception cref="System.ArgumentNullException">context</exception>
+		public override void OnException(HttpActionExecutedContext actionExecutedContext)
+		{
+			if (ReferenceEquals(actionExecutedContext, null))
+			{
+				throw new ArgumentNullException(nameof(actionExecutedContext));
+			}
 
-            Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(actionExecutedContext.Exception));
-        }
-    }
+			Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(actionExecutedContext.Exception));
+		}
+	}
 }
--- GenBOE/RDSB.Web/Controllers/RdsbDataApiController.cs
+++ GenBOE/RDSB.Web/Controllers/RdsbDataApiController.cs
@@ -19,116 +19,116 @@ namespace RDSB.Web.Controllers
 	/// RDSB Data API Controller - used to serve up RDSB data for ACV (or other applications as needed)
 	/// </summary>
 	[AllowAnonymous]
-    public class RdsbDataApiController : ApiController
-    {
-        #region Properties & Ctor
+	public class RdsbDataApiController : ApiController
+	{
+		#region Properties & Ctor
 
-        /// <summary>
-        /// Security Information
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private ISecurityInformation security;
+		/// <summary>
+		/// Security Information
+		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+		private ISecurityInformation security;
 
-        /// <summary>
-        /// Token Handling
-        /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
-        private TokenHandling tokenHandler;
+		/// <summary>
+		/// Token Handling
+		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+		private TokenHandling tokenHandler;
 
-        /// <summary>
-        /// Logger
-        /// </summary>
-        private Logger logger = new Logger("RdsbDataAPIController");
+		/// <summary>
+		/// Logger
+		/// </summary>
+		private Logger logger = new Logger("RdsbDataAPIController");
 
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="security">Security Information</param>
-        /// <param name="tokenHandler">Token Handling</param>
-        public RdsbDataApiController(ISecurityInformation security, TokenHandling tokenHandler)
-        {
-            this.security = security;
-            this.tokenHandler = tokenHandler;
-        }
+		/// <summary>
+		/// ctor
+		/// </summary>
+		/// <param name="security">Security Information</param>
+		/// <param name="tokenHandler">Token Handling</param>
+		public RdsbDataApiController(ISecurityInformation security, TokenHandling tokenHandler)
+		{
+			this.security = security;
+			this.tokenHandler = tokenHandler;
+		}
 
-        #endregion
+		#endregion
 
-        /// <summary>
-        /// Check if an RDSB record exists for the given PTM Tracking Number
-        /// </summary>
-        /// <param name="ptmTrackingNumber">PTM Tracking Number</param>
-        /// <returns>true if record exists, otherwise false</returns>
-        [HttpGet]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "ptmTrackingNumber")]
-        public IESResponse<bool> DoesRdsbRecordExist(string ptmTrackingNumber)
-        {
-            IESResponse<bool> toReturn = new IESResponse<bool>();
+		/// <summary>
+		/// Check if an RDSB record exists for the given PTM Tracking Number
+		/// </summary>
+		/// <param name="ptmTrackingNumber">PTM Tracking Number</param>
+		/// <returns>true if record exists, otherwise false</returns>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "ptmTrackingNumber")]
+		public IESResponse<bool> DoesRdsbRecordExist(string ptmTrackingNumber)
+		{
+			IESResponse<bool> toReturn = new IESResponse<bool>();
 
-            // TODO - check record
+			// TODO - check record
 
-            return toReturn;
-        }
+			return toReturn;
+		}
 
-        /// <summary>
-        /// Export the RDSB Document
-        /// </summary>
-        /// <param name="ptmTrackingNumber">PTM Tracking Number</param>
-        /// <param name="parentSectionNumber">Parent Section Number</param>
-        /// <returns>RDSB Document in HTTP Response Message</returns>
-        [HttpGet]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "ptmTrackingNumber")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "parentSectionNumber")]
-        public HttpResponseMessage ExportRdsbDocument(string ptmTrackingNumber, int parentSectionNumber)
-        {
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+		/// <summary>
+		/// Export the RDSB Document
+		/// </summary>
+		/// <param name="ptmTrackingNumber">PTM Tracking Number</param>
+		/// <param name="parentSectionNumber">Parent Section Number</param>
+		/// <returns>RDSB Document in HTTP Response Message</returns>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "ptmTrackingNumber")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "parentSectionNumber")]
+		public HttpResponseMessage ExportRdsbDocument(string ptmTrackingNumber, int parentSectionNumber)
+		{
+			HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
 
-            try
-            {
-                MemoryStream stream = new MemoryStream();
-                string fileName = string.Empty;
+			try
+			{
+				MemoryStream stream = new MemoryStream();
+				string fileName = string.Empty;
 
-                // TODO - export, name file
+				// TODO - export, name file
 
-                stream.Position = 0; // We need to set this to return the file
-                response.Content = new StreamContent(stream);
-                response.Content.Headers.ContentType = new MediaTypeHeaderValue(ExportFileDownloadBase.ContentType_DOCX);
-                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
-                {
-                    FileName = fileName
-                };
-            }
-            catch (Exception ex)
-            {
-                this.logger.Error(ex);
-                response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
-            }
+				stream.Position = 0; // We need to set this to return the file
+				response.Content = new StreamContent(stream);
+				response.Content.Headers.ContentType = new MediaTypeHeaderValue(ExportFileDownloadBase.ContentType_DOCX);
+				response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+				{
+					FileName = fileName
+				};
+			}
+			catch (Exception ex)
+			{
+				this.logger.Error(ex);
+				response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+			}
 
-            return response;
-        }
+			return response;
+		}
 
-        /// <summary>
-        /// Is Service Alive?
-        /// </summary>
-        /// <returns>True/false</returns>
-        [HttpGet]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        public bool IsAlive()
-        {
-            bool result;
+		/// <summary>
+		/// Is Service Alive?
+		/// </summary>
+		/// <returns>True/false</returns>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public bool IsAlive()
+		{
+			bool result;
 
-            try
-            {
-                // ToDo: add a DB grab, just to see if the DB is working.. To make the check more meaningful
-                result = true;
-            }
-            catch
-            {
-                result = false;
-            }
+			try
+			{
+				// ToDo: add a DB grab, just to see if the DB is working.. To make the check more meaningful
+				result = true;
+			}
+			catch
+			{
+				result = false;
+			}
 
-            return result;
-        }
-    }
+			return result;
+		}
+	}
 }
