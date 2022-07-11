096f1ab0f23b1dc6b24ac30ffcb6a24071efde78
Timothy Wilson <timothy.wilson@lmco.com>
2022-07-11 17:43:54 -0400

    Null check for byte[] data Remove commented out code

--- GenBOE/GenBOE.Web/Resources/js/angular/MoqEquationController.js
+++ GenBOE/GenBOE.Web/Resources/js/angular/MoqEquationController.js
@@ -1015,41 +1015,14 @@ moqEquationApp.controller('MoqEquationController', ['$scope', '$document', '$uib
 				}
 			}
 			else {
-				//if (navigator.msSaveBlob) {
-				//	var blob = new Blob([response.data.Data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-				//	return navigator.msSaveBlob(blob, filename);
-				//} else {
-				//	var link = document.createElement('a');
-				//	link.style.display = 'none';
-				//	link.setAttribute('target', '_blank');
-				//	link.setAttribute('href', 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8,' + encodeURIComponent(response.data.Data));
-				//	link.setAttribute('download', 'ActualsExport.xlsx');
-				//	document.body.appendChild(link);
-				//	link.click();
-				//	document.body.removeChild(link);
-				//}
-
 				var blob = $scope.b64toBlob(response.data.Data, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-
-				//// attach the file to the document body
-				//var blob = new Blob(response.data.Data, {
-				//	type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-				//});
 				var filename = 'ActualsExport.xlsx';
 				
-
 				if (navigator.msSaveBlob)
 					navigator.msSaveBlob(blob, filename);
 				else {
-					//// Try using other saveBlob implementations, if available
-					//var saveBlob = navigator.webkitSaveBlob || navigator.mozSaveBlob || navigator.saveBlob;
-					//if (saveBlob === undefined) throw "Not supported";
-					//saveBlob(blob, filename);
 					// trick to download store a file having its URL
 					var fileURL = URL.createObjectURL(blob);
-
-					// window.location = blobUrl;
-
 					var a = document.createElement('a');
 					a.href = fileURL;
 					a.target = '_blank';
