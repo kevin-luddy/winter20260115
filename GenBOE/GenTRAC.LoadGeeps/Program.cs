7f361487760645a469950a88b43b98c048a72428
Dusan <dusan.palider@lmco.com>
2022-07-19 09:48:23 -0400

    Yearly Oracle password change

--- GenBOE/GenTRAC.LoadGeeps/Program.cs
+++ GenBOE/GenTRAC.LoadGeeps/Program.cs
@@ -69,7 +69,7 @@ namespace GenTRAC.LoadGeeps
         private static ICollection<DataMartEmployeeDTO> LoadEmployees()
         {
             List<DataMartEmployeeDTO> employees = new List<DataMartEmployeeDTO>();
-            using (OracleConnection conn = new OracleConnection("Data Source=DWLMP;User Id=GEEPS_PTM;Password=etadpuSLP_24;"))
+            using (OracleConnection conn = new OracleConnection("Data Source=DWLMP;User Id=GEEPS_PTM;Password=mM_6SYm_ppGoew;"))
             {
                 conn.Open();
 
