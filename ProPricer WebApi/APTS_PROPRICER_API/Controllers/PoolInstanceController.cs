17700deac04f7e9c3923407ac5bb55394da5aff2
Timothy Wilson <timothy.wilson@lmco.com>
2022-07-26 14:38:16 -0400

    Added xml and moved usings

--- ProPricer WebApi/APTS_PROPRICER_API/Controllers/PoolInstanceController.cs
+++ ProPricer WebApi/APTS_PROPRICER_API/Controllers/PoolInstanceController.cs
@@ -7,13 +7,16 @@
     and is not to be made available to third parties without the prior written permission of Lockheed Martin Corporation.
 */
 
-using System;
-using System.Collections.Generic;
-using APTSPropricerApi.Connection;
-using APTSPropricerApi.DTOs;
-
 namespace APTSPropricerApi.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using APTSPropricerApi.Connection;
+    using APTSPropricerApi.DTOs;
+
+    /// <summary>
+    /// The Pool Instance Controller
+    /// </summary>
     public class PoolInstanceController : ProPricerController
     {
         // GET api/poolinstance
