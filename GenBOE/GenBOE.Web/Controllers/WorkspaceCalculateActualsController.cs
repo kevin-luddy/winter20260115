e63ccd84856965131d029b7e70af3e706e3c1ece
Timothy Wilson <timothy.wilson@lmco.com>
2022-09-26 10:03:52 -0400

    code review updates

--- GenBOE/GenBOE.Web/Controllers/WorkspaceCalculateActualsController.cs
+++ GenBOE/GenBOE.Web/Controllers/WorkspaceCalculateActualsController.cs
@@ -6,48 +6,53 @@
 
 namespace GenBOE.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Transactions;
-    using System.Web.Mvc;
-    using System.Web.Script.Serialization;
-    using ActionLogic.ModelView.Clin;
-    using GenBOE.ActionLogic;
-    using GenBOE.ActionLogic.BLL;
-    using GenBOE.ActionLogic.BOETransitions;
-    using GenBOE.ActionLogic.Common;
-    using GenBOE.ActionLogic.Common.Email;
-    using GenBOE.ActionLogic.ControllerLogic;
-    using GenBOE.ActionLogic.IO.Export;
-    using GenBOE.ActionLogic.IO.Import;
-    using GenBOE.ActionLogic.Metrics;
-    using GenBOE.ActionLogic.ModelView;
-    using GenBOE.ActionLogic.Validation;
-    using GenBOE.ActionLogic.WBS;
-    using GenBOE.DataBridge.Common;
-    using GenBOE.DataBridge.Common.Interfaces;
-    using GenBOE.DataBridge.DTO;
-    using GenBOE.Dtos;
-    using GenBOE.Objects;
-    using GenBOE.Web.Common;
-    using GenBOE.Web.ModelView;
-    using IES.Common;
-    using IES.Common.Exceptions;
-    using IES.Common.OfficeUtilities;
+	using System.Diagnostics;
+	using System.Web.Mvc;
+	using GenBOE.ActionLogic.BLL;
+	using GenBOE.ActionLogic.BOETransitions;
+	using GenBOE.ActionLogic.Common;
+	using GenBOE.ActionLogic.ControllerLogic;
+	using GenBOE.ActionLogic.Metrics;
+	using GenBOE.DataBridge.Common;
+	using GenBOE.DataBridge.Common.Interfaces;
+	using GenBOE.DataBridge.DTO;
+	using GenBOE.Objects;
+	using GenBOE.Web.Common;
+	using IES.Common;
 
-    public class WorkspaceCalculateActualsController : GenBOEController
+	/// <summary>
+	/// Workspace Actuals Controller
+	/// </summary>
+	public class WorkspaceCalculateActualsController : GenBOEController
     {
+		/// <summary>
+		/// Logger
+		/// </summary>
         private readonly Logger _log = new Logger(typeof(WBSController));
 
+		/// <summary>
+		/// boe state machine
+		/// </summary>
         private readonly BOEStateMachine _BOEStateMachine = null;
+
+		/// <summary>
+		/// boe mediator
+		/// </summary>
         private readonly BoeMediator _BoeMediator = null;
         
         /// <summary>
         /// Constructor
         /// </summary>
+		/// <param name="factory">Full object factory</param>
+		/// <param name="inBoeMediator">Boe mediator</param>
+		/// <param name="inBOEStateMachine">Boe state machine</param>
+		/// <param name="inCommonDataMapper">Common data mapper</param>
+		/// <param name="inControllerLogic">boe controller logic</param>
+		/// <param name="inSecurityAccess">Security access</param>
+		/// <param name="inSiteMasterUtilities">Site master utilities</param>
+		/// <param name="inSystemMetrics">System metrics</param>
+		/// <param name="permissionLoader">Permission loader</param>
+		/// <param name="userLoader">User loader</param>
         public WorkspaceCalculateActualsController(ISecurityAccess inSecurityAccess,
             CommonDataMapper inCommonDataMapper,
             SiteMasterUtilities inSiteMasterUtilities,
@@ -69,7 +74,7 @@ namespace GenBOE.Web.Controllers
         /// Returns the WorkspaceCalculateActuals view
         /// </summary>
         /// <param name="workspace"></param>
-        /// <returns></returns>
+        /// <returns>Main page for workspace calculate actuals</returns>
         public ViewResult Index(string workspace)
         {
             FullWorkspace ws = this.Factory.CreateFullWorkspace(workspace);
@@ -90,10 +95,10 @@ namespace GenBOE.Web.Controllers
 		#region Display
 
 		/// <summary>
-		/// Get the Manage WBS Grid MV
+		/// Get the Workspace Calculate Actuals Model for grid
 		/// </summary>
 		/// <param name="workspace">the workspace</param>
-		/// <returns>The MV for the Manage WBS grid</returns>
+		/// <returns>The MV for the Workspace Calculate Actuals grid</returns>
 		public JsonResult GetWorkspaceCalculateActualsModel(string workspace)
 		{
 			FullWorkspace ws = this.Factory.CreateFullWorkspace(workspace);
@@ -114,10 +119,5 @@ namespace GenBOE.Web.Controllers
 		}
 
 		#endregion Display
-
-		#region AJAX Calls
-
-
-		#endregion
 	}
 }
