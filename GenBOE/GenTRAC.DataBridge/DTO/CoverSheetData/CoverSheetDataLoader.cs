b1b2b7fbfd9e3dd8386f5d4f0232ee1399c65edd
Dusan <dusan.palider@lmco.com>
2022-10-23 20:28:10 -0400

    IES-1965: Fixed how PTM data is being retrieved for ACV;

--- GenBOE/GenTRAC.DataBridge/DTO/CoverSheetData/CoverSheetDataLoader.cs
+++ GenBOE/GenTRAC.DataBridge/DTO/CoverSheetData/CoverSheetDataLoader.cs
@@ -8,10 +8,7 @@ namespace GenTRAC.DataBridge.DTO
 {
     using System.Collections.Generic;
     using System.Linq;
-    using GenTRAC.DataBridge.DTO.Contracts;
     using GenTRAC.Models;
-    using GenTRAC.Objects;
-    using GenTRAC.Objects.FullObject;
     using IES.Common;
 
     /// <summary>
@@ -25,39 +22,11 @@ namespace GenTRAC.DataBridge.DTO
         public Logger Log { get; }
 
         /// <summary>
-        /// Gets or sets the Proposal Loader.
-        /// </summary>
-        private IProposalLoader ProposalLoader { get; set; }
-
-        /// <summary>
-        /// Object Factory
-        /// </summary>
-        protected IFullObjectFactory ObjectFactory { get; set; }
-
-        /// <summary>
-        /// The user mapper
-        /// </summary>
-        protected IUserMapper UserMapper { get; set; }
-
-        /// <summary>
         /// Default Constructor
         /// </summary>
         public CoverSheetDataLoader()
         {
             this.Log = new Logger(typeof(CoverSheetDataLoader));
-        }
-
-        /// <summary>
-        /// Default constructor
-        /// </summary>
-        /// <param name="proposalLoader">Proposal Loader</param>
-        /// <param name="objectFactory">Object Factory</param>
-        /// <param name="userMapper">User Mapper</param>
-        public CoverSheetDataLoader(IProposalLoader proposalLoader, IFullObjectFactory objectFactory, IUserMapper userMapper) : this()
-        {
-            this.ProposalLoader = proposalLoader;
-            this.ObjectFactory = objectFactory;
-            this.UserMapper = userMapper;
         }
 
         /// <summary>
@@ -73,69 +42,29 @@ namespace GenTRAC.DataBridge.DTO
             {
                 using (genTRACEntities dbModel = new genTRACEntities())
                 {
-                    coverSheetData = (from props in dbModel.Proposals
-                                      join pcl in dbModel.ProposalChecklists on props.ProposalID equals pcl.ProposalID into propspcl
-                                      from pcl in propspcl.DefaultIfEmpty()
-                                      join pc in dbModel.ProposalContractsDatas on (pcl == null ? 0 : pcl.ProposalID) equals pc.ProposalID into propspc
-                                      from pc in propspc.DefaultIfEmpty()
-                                      where props.ProposalID == id
-                                      select new CoverSheetDataDto
-                                      {
-                                          IsCCPDRequired = props.CCPDRequired,
-                                          ContractActionType = (ContractActionType?)props.ContractActionType,
-                                          OtherContractActionType = props.ContractActionTypeOtherText,
-                                          ContractTypeGroup = props.ContractTypeGroupID.HasValue ? props.ContractTypeGroupID.Value : 0,
-                                          CoverSheetApproverSignedDate = props.CoverSheetApproverSignedDT,
-                                          CostThroughCom = pcl.CostThroughCom,
-                                          ProfitFee = pcl.Profit,
-                                          LMSpaceTotalPrice = pcl.ISGSTotalPrice,
-                                          CustomerSubmittalDate = pc.CustomerSubmittalDate,
-                                          CageCode = pc.CageCode
-                                      }).FirstOrDefault();
-
-                    if (coverSheetData.CageCode != null)
-                    {
-                        // Get data for offeror's address from Cage Codes
-                        CageCodeDTO cageCodeData = dbModel.CageCodes.Where(x => x.CageCode1 == coverSheetData.CageCode).Select(x => new CageCodeDTO()
+                    coverSheetData = dbModel.Proposals.Where(x => x.ProposalID == id).Select(prop => new CoverSheetDataDto
                         {
-                            Address1 = x.Address1,
-                            Address2 = x.Address2,
-                            City = x.City,
-                            State = x.State,
-                            Zip = x.Zip,
-                        }).FirstOrDefault();
-
-                        coverSheetData.OfferorAddress = new List<string>() { cageCodeData.Address1, cageCodeData.Address2, cageCodeData.City, cageCodeData.State, cageCodeData.Zip };
-                    }
-
-                    FullProposal fullProposal = ObjectFactory.CreateFullProposal(ProposalLoader.GetById(id));
-
-                    if (fullProposal != null)
-                    {
-                        // Get Cover Sheet Approver and Contracts Lead NTID by creating full proposal and checking permissions
-                        foreach (ProposalPermissionDto permission in fullProposal.Permissions)
-                        {
-                            UserDTO user = this.UserMapper.GetById(permission.UserId);
-
-                            switch (permission.Role)
-                            {
-                                case PtmRole.CoverSheetApprover:
-                                    if (user != null)
-                                    {
-                                        coverSheetData.CoverSheetApproverNtid = user.Ntid;
-                                    }
-                                    break;
-                                case PtmRole.ContractsPOC:
-                                    if (user != null)
-                                    {
-                                        coverSheetData.ContractsLead = user.Ntid;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    }
+                            IsCCPDRequired = prop.CCPDRequired,
+                            ContractActionType = (ContractActionType?)prop.ContractActionType,
+                            OtherContractActionType = prop.ContractActionTypeOtherText,
+                            ContractTypeGroup = prop.ContractTypeGroupID ?? 0,
+                            CoverSheetApproverSignedDate = prop.CoverSheetApproverSignedDT,
+                            CostThroughCom = prop.ProposalChecklists.FirstOrDefault().CostThroughCom,
+                            ProfitFee = prop.ProposalChecklists.FirstOrDefault().Profit,
+                            LMSpaceTotalPrice = prop.ProposalChecklists.FirstOrDefault().ISGSTotalPrice,
+                                          
+                            CustomerSubmittalDate = prop.ProposalContractsDatas1.FirstOrDefault().CustomerSubmittalDate,
+                            CageCode = prop.ProposalContractsDatas1.FirstOrDefault().CageCode,
+                            OfferorAddress = new List<string>() {
+                                prop.ProposalContractsDatas1.FirstOrDefault().CageCode1.Address1,
+								prop.ProposalContractsDatas1.FirstOrDefault().CageCode1.Address2,
+								prop.ProposalContractsDatas1.FirstOrDefault().CageCode1.City,
+								prop.ProposalContractsDatas1.FirstOrDefault().CageCode1.State,
+								prop.ProposalContractsDatas1.FirstOrDefault().CageCode1.Zip
+                            },
+                            CoverSheetApproverNtid = prop.ProposalUserRoles.FirstOrDefault(x => x.RoleID == (int)PtmRole.CoverSheetApprover).genTRACUser.NTID,
+							ContractsLead = prop.ProposalUserRoles.FirstOrDefault(x => x.RoleID == (int)PtmRole.ContractsPOC).genTRACUser.NTID
+						}).FirstOrDefault();
                 }
             }
 
