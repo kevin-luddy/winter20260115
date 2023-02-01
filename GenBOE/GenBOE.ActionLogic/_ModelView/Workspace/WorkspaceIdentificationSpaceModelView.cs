3f4751b5e3ecfe66438a0020c897eee1e8a23dd4
Asuncion, Thomas (US) <thomas.asuncion@lmco.com>
2023-02-01 09:28:14 -0700

    Merged PR 1125: ACV-221: Fix Display for "Enable SAP Connection" in Workspace Identification Page
    Inside of the Workspace Identification page after the Workspace was created, the "Enable SAP Connection" field was not properly being populated with the value from the database. This fixes that as the problem was inside of the Model View.
    
    Thanks Tim for pointing this out!

--- GenBOE/GenBOE.ActionLogic/_ModelView/Workspace/WorkspaceIdentificationSpaceModelView.cs
+++ GenBOE/GenBOE.ActionLogic/_ModelView/Workspace/WorkspaceIdentificationSpaceModelView.cs
@@ -48,6 +48,7 @@ namespace GenBOE.ActionLogic.ModelView.Workspace
                 this.TrackingNumber = workspaceDTO.TrackingNumber;
                 this.IsUsingTM = workspaceDTO.IsUsingTM;
                 this.ProposalTitle = workspaceDTO.ProposalTitle;
+                this.EnableSAPConnection = workspaceDTO.EnableSAPConnection;
                 this.RevisedSubmittalDate = workspaceDTO.RevisedSubmittalDate.HasValue?
                     workspaceDTO.RevisedSubmittalDate.Value.ToString("MM/dd/yyyy") : 
                     string.Empty;
