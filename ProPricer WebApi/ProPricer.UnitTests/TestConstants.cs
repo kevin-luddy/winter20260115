13aad597239c5c0eb5378d358b7df62a2a8ff7eb
Timothy Wilson <timothy.wilson@lmco.com>
2023-02-10 14:09:24 -0500

    Add copyright fix formatting

--- ProPricer WebApi/ProPricer.UnitTests/TestConstants.cs
+++ ProPricer WebApi/ProPricer.UnitTests/TestConstants.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+    Copyright 2016-2020 Lockheed Martin Corporation.
+
+    This computer software has been provided in confidence, and contains trade secret and/or privileged or confidential 
+    commercial or financial information. Public disclosure of any information marked as indicated above is prohibited 
+    by the Trade Secrets Act (18 U.S.C. Sec. 1905) and the Economic Espionage Act of 1996 (18 U.S.C. Sec. 1831 et seq.) 
+    and is not to be made available to third parties without the prior written permission of Lockheed Martin Corporation.
+*/
 
 namespace UnitTestProject
 {
-    /// <summary>
-    /// Constants for Unit Tests
-    /// </summary>
-    public static class TestConstants
-    {
-        /// <summary>
-        /// The RMS instance identifier
-        /// </summary>
-        public static readonly int InstanceId = 2;
+	/// <summary>
+	/// Constants for Unit Tests
+	/// </summary>
+	public static class TestConstants
+	{
+		/// <summary>
+		/// The RMS instance identifier
+		/// </summary>
+		public static readonly int InstanceId = 2;
 
-        /// <summary>
-        /// The Space instance identifier
-        /// </summary>
-        public static readonly int SpaceInstanceId = 1;
-    }
+		/// <summary>
+		/// The Space instance identifier
+		/// </summary>
+		public static readonly int SpaceInstanceId = 1;
+	}
 }
--- ProPricer WebApi/ProPricerWebApi/Common/TokenAuthenticationSchemeHandler.cs
+++ ProPricer WebApi/ProPricerWebApi/Common/TokenAuthenticationSchemeHandler.cs
@@ -1,15 +1,23 @@
-﻿
+﻿/*
+    Copyright 2016-2020 Lockheed Martin Corporation.
+
+    This computer software has been provided in confidence, and contains trade secret and/or privileged or confidential 
+    commercial or financial information. Public disclosure of any information marked as indicated above is prohibited 
+    by the Trade Secrets Act (18 U.S.C. Sec. 1905) and the Economic Espionage Act of 1996 (18 U.S.C. Sec. 1831 et seq.) 
+    and is not to be made available to third parties without the prior written permission of Lockheed Martin Corporation.
+*/
+
 namespace APTSPropricerApi.Common
 {
-	using Microsoft.AspNetCore.Authentication;
-	using Microsoft.Extensions.Options;
-	using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-	using Microsoft.IdentityModel.Protocols;
-	using Microsoft.IdentityModel.Tokens;
 	using System.IdentityModel.Tokens.Jwt;
 	using System.Security.Claims;
 	using System.Security.Principal;
 	using System.Text.Encodings.Web;
+	using Microsoft.AspNetCore.Authentication;
+	using Microsoft.Extensions.Options;
+	using Microsoft.IdentityModel.Protocols;
+	using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+	using Microsoft.IdentityModel.Tokens;
 
 	/// <summary>
 	/// IES Token Scheme Handler
