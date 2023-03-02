3e70a0ee7c0db91c05b0d23d210239e78c7c7360
Timothy Wilson <timothy.wilson@lmco.com>
2023-03-02 17:47:56 -0500

    update version table too

--- GenBOE.Database/Scripts/Release Scripts/Release 2023.4/Release 2023.4.sql
+++ GenBOE.Database/Scripts/Release Scripts/Release 2023.4/Release 2023.4.sql
@@ -13,6 +13,12 @@ GO
 ALTER TABLE dbo.MOQTypeSelectionTableData ALTER COLUMN TotalWbsHours [decimal](11,2) NOT NULL
 GO
 
+ALTER TABLE [version].MOQTypeSelectionTableData ALTER COLUMN TotalRelevantHoursAfterQueryFilters [decimal](11,2) NOT NULL
+GO
+
+ALTER TABLE [version].MOQTypeSelectionTableData ALTER COLUMN TotalWbsHours [decimal](11,2) NOT NULL
+GO
+
 
 /*
     3/1/23		twilson3			ACV-343 Update MOQ Column sizes
