0bae3ccb16fa0a8fc3ceff8f11202d3f8cb4ad44
RJ <ronald.anzalone@lmco.com>
2023-03-13 17:40:24 -0400

    ACV-418 - fix employee id suppression for ssc

--- GenBOE/GenBOE.ActionLogic/IO/Export/WordExporter.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/WordExporter.cs
@@ -699,7 +699,8 @@ namespace GenBOE.ActionLogic.IO.Export
 						else
 						{
 							// mask the employee ids
-							WordUtilities.SetElementText(WordUtilities.GetTaggedChildElement(moqTypeTableContainer, BOEExporterConstants.FieldName_AdditionalQueryFilters), MaskSpaceEmployeeIds(EmployeeIdFilters, table.AdditionalQueryFilters));
+							WordUtilities.SetElementText(WordUtilities.GetTaggedChildElement(moqTypeTableContainer, BOEExporterConstants.FieldName_AdditionalQueryFilters), 
+                                exportInputs.Workspace.EnableSAPConnection && table.RepositoryName == RepositoryName.SapWebi.GetDescription() ? MaskSpaceEmployeeIds(EmployeeIdFilters, table.AdditionalQueryFilters) : BOEExporterConstants.EMPLOYEE_ID_FILTERS_EXCLUSION_TEXT);
 						}
 					}
 					else
