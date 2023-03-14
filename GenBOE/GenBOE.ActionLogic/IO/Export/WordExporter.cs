173bf76de437705a3766623b48da88f6389e4192
RJ <ronald.anzalone@lmco.com>
2023-03-14 10:21:36 -0400

    ACV-418 - CR feedback - check web config sap setting

--- GenBOE/GenBOE.ActionLogic/IO/Export/WordExporter.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/WordExporter.cs
@@ -699,8 +699,8 @@ namespace GenBOE.ActionLogic.IO.Export
 						else
 						{
 							// mask the employee ids
-							WordUtilities.SetElementText(WordUtilities.GetTaggedChildElement(moqTypeTableContainer, BOEExporterConstants.FieldName_AdditionalQueryFilters), 
-                                exportInputs.Workspace.EnableSAPConnection && table.RepositoryName == RepositoryName.SapWebi.GetDescription() ? MaskSpaceEmployeeIds(EmployeeIdFilters, table.AdditionalQueryFilters) : BOEExporterConstants.EMPLOYEE_ID_FILTERS_EXCLUSION_TEXT);
+							WordUtilities.SetElementText(WordUtilities.GetTaggedChildElement(moqTypeTableContainer, BOEExporterConstants.FieldName_AdditionalQueryFilters),
+								Utilities.IsSAPEnabled && exportInputs.Workspace.EnableSAPConnection && table.RepositoryName == RepositoryName.SapWebi.GetDescription() ? MaskSpaceEmployeeIds(EmployeeIdFilters, table.AdditionalQueryFilters) : BOEExporterConstants.EMPLOYEE_ID_FILTERS_EXCLUSION_TEXT);
 						}
 					}
 					else
