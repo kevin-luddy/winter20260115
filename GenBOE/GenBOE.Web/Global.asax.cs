86141ff273804ac0c6c0be6209413a77cf04e4ef
Timothy Wilson <timothy.wilson@lmco.com>
2023-03-16 15:25:11 -0400

    Update timeout to be 5 minutes for SAP API client Update Token Authorization to be only added if different to SAP Client, and added locking

--- GenBOE/GenBOE.Web/Global.asax.cs
+++ GenBOE/GenBOE.Web/Global.asax.cs
@@ -438,6 +438,7 @@ namespace GenBOE
                 new ResolvedParameter(typeof(IBoeDTODataLoader)),
                 new ResolvedParameter(typeof(IPermissionsDTODataLoader))));
 
+			_SapHttpClient.Timeout = TimeSpan.FromMinutes(5);
 			GenBOEUnityContainer.Container.RegisterType(typeof(GenBOE.ActionLogic.IESSAPClient.IESSAPClient), typeof(GenBOE.ActionLogic.IESSAPClient.IESSAPClient), GetLifetimeManager(), new InjectionConstructor(ConfigurationUtilities.GetAppSetting("IESSAPUrl"), _SapHttpClient));
 
             // Controller Logic
