21121face279ef2a58034aa47687dd2ab4adbb80
Hazrat Rafiqzadah <hazrat.b.rafiqzadah@lmco.com>
2023-04-19 11:25:18 -0400

    Remove unnecessary using statement

--- GenBOE/GenBOE.ModelView/DTO/Workspace/WorkspaceDTODataLoader.cs
+++ GenBOE/GenBOE.ModelView/DTO/Workspace/WorkspaceDTODataLoader.cs
@@ -11,7 +11,6 @@ namespace GenBOE.DataBridge.DTO
 	using System.Collections.ObjectModel;
 	using System.Data.SqlClient;
 	using System.Linq;
-	using System.Security.Policy;
 	using GenBOE.Dtos;
 	using GenBOE.Models;
 	using IES.Common;
