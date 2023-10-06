73fed68771ffa34637f0acbc1e8d640e5076744b
Wilson, Timothy <timothy.wilson@lmco.com>
2023-10-06 11:43:10 -0400

    remove commented lines

--- GenBOE/GenBOE.ActionLogic/IO/Export/PBOEFormExporter.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/PBOEFormExporter.cs
@@ -217,12 +217,8 @@ namespace GenBOE.ActionLogic.IO.Export
             this.SetScheduleEventDateField(document, GOVT_PRICING_CCOPD_REVIEW, boeForm.GovtPricingReceived == null ? ScheduleEvent.NA : (ScheduleEvent)boeForm.GovtPricingReceived, boeForm.GovtPricingReceivedDate, boeForm?.GovtPricingReceivedText);
             this.SetScheduleEventDateField(document, COST_ANALYSIS_UNQUALIFIED, boeForm.CostAnalysisUnqual == null ? ScheduleEvent.NA : (ScheduleEvent)boeForm.CostAnalysisUnqual, boeForm.CostAnalysisUnqualDate, boeForm?.CostAnalysisUnqualText);
             this.SetDateField(document, PLANNED_DATE_A, boeForm.PlannedDate_WrittenApproval);
-            //if (boeForm.BOEFormType != BOEFormType.PBOE || boeForm.Revision < 2)
-            //{
-            //    // This field was removed for PBOE revision 2
-                this.SetDateField(document, PLANNED_DATE_B, boeForm.PlannedDate_ApprovedSubmission);
-            //}
-
+            this.SetDateField(document, PLANNED_DATE_B, boeForm.PlannedDate_ApprovedSubmission);
+            
             // IES-1017: New RTE Fields
             this.SetHtmlField(document, PROPOSED_COMMERCIALITY, boeForm.CommercialityDescription, ref counters);
             this.SetHtmlField(document, PROPOSED_SOURCE_SELECTION, boeForm.SourceSelectionDescription, ref counters);
