18b23c3338a7e155dcee9e90d58f6ab409c7c2a7
Wilson, Timothy <timothy.wilson@lmco.com>
2023-11-01 19:07:42 -0400

    remove limiter code

--- ProPricer WebApi/ProPricerWebApi/Controllers/ProPricerDataApiController.cs
+++ ProPricer WebApi/ProPricerWebApi/Controllers/ProPricerDataApiController.cs
@@ -61,7 +61,7 @@ namespace APTSPropricerApi.Controllers
 			{
 				using (IProPricerConnection ppc = (IProPricerConnection)poolManagerList.GetInstance(instanceId).GetObjectsFromPool())
 				{
-					response.Data = Utility.GetAllProposals(ppc, logger).Take(500).ToList();
+					response.Data = Utility.GetAllProposals(ppc, logger);
 					response.IsSuccessful = true;
 				}
 			}
