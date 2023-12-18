93626764172827b64ff3ed167825a4c5f88547ca
Breanne <breanne.m.nowicki@lmco.com>
2023-12-18 13:51:51 -0700

    Fixed the Contract Type Group display to text for the read only view in PTM Proposals

--- GenBOE/GenTRAC.ActionLogic/ModelView/Proposals/ProposalInformationModelView.cs
+++ GenBOE/GenTRAC.ActionLogic/ModelView/Proposals/ProposalInformationModelView.cs
@@ -134,7 +134,7 @@ namespace GenTRAC.ActionLogic.ModelView.Proposals
         /// Gets or sets Contract Type Group
         /// </summary>
         [Required(ErrorMessage = ValidationConstants.ProposalValidationConstants.CONTRACT_TYPE_GROUP_REQUIRED)]
-        public int ContractTypeGroup { get; set; }
+        public int? ContractTypeGroup { get; set; }
 
         /// <summary>
         /// Gets or sets Contract Type
@@ -172,7 +172,7 @@ namespace GenTRAC.ActionLogic.ModelView.Proposals
 
         /// <summary>
         /// List of allowed proposal classes
-        /// </summary>
+        /// </summary>T
         public ICollection<SelectListItem> ProposalClassesList { get; set; }
 
         /// <summary>
@@ -228,10 +228,15 @@ namespace GenTRAC.ActionLogic.ModelView.Proposals
         /// </summary>
         public string RequestTypeText { get; set; }
 
-        /// <summary>
-        /// Text for Proposal Class (used by read only
-        /// </summary>
-        public string ProposalClassText { get; set; }
+		/// <summary>
+		/// Text for Contract Type Group (used by read only
+		/// </summary>
+		public string ContractTypeGroupText { get; set; }
+
+		/// <summary>
+		/// Text for Proposal Class (used by read only
+		/// </summary>
+		public string ProposalClassText { get; set; }
 
         /// <summary>
         /// Document Id (RDSB)
