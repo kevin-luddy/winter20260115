293e9c925aca4c36e81457dac1a2d69b226a0044
Wilson, Timothy <timothy.wilson@lmco.com>
2024-01-30 13:16:32 -0500

    Fix DB release

--- GenBOE.Database/Scripts/Release Scripts/Release 2024.2/1 - Release 2024.2 Script.sql
+++ GenBOE.Database/Scripts/Release Scripts/Release 2024.2/1 - Release 2024.2 Script.sql
@@ -1,4 +1,4 @@
-EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2024.1';
+EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2024.2';
 GO
 
 IF COL_LENGTH ('dbo.MOQTypeSelection', 'HistoricalReferenceExplanation') IS NULL
