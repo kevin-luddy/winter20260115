ebce986b64c349f8efec9930fd4f9f509e8f735f
Lutz <e419085@us.lmco.com>
2024-02-22 14:25:57 -0700

    removed stored procedures

--- GenBOE.Database/PTM/_Full Script Files For Specific Versions/PTM - 2024.5 Generated On 2_22_2024.sql
+++ GenBOE.Database/PTM/_Full Script Files For Specific Versions/PTM - 2024.5 Generated On 2_22_2024.sql
@@ -1,6 +1,6 @@
 PRINT '###### SCRIPT IS STARTING ######';
 /*
-    This file was auto-generated for Release: 2024.5, on 2/21/2024.
+    This file was auto-generated for Release: 2024.5, on 2/22/2024.
     It contains all of the Release specific scripts, modifying data/tables as well as all of the Stored Procedures and User Defined Table Types.
 */
 
@@ -16,115 +16,6 @@ GO
 
                 2/19/24 [Scott] - PROPH-1558 - Update PPR Checklist
 */
-
--- Create the new SPs here because release scripts run before SP scripts
-IF  EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CopyPARChecklist]') AND type in (N'P', N'PC'))
-	DROP PROCEDURE [dbo].[CopyPARChecklist];
-GO
-
-CREATE PROCEDURE dbo.CopyPARChecklist
-(@newChecklistId AS INT) AS
-/******************************************************************************
-**		 
-**		Name: CopyPARChecklist
-**		Desc: Copies PAR Checklist into a new Checklist version. This is executed manually.
-**
-**		Auth: RJ
-**		Date: 4/13/23
-*******************************************************************************
-**		Change History
-*******************************************************************************
-**		Date:		Author:				Description:
-**		--------	--------			---------------------------------------
-*******************************************************************************/
-BEGIN
-	IF NOT EXISTS (SELECT 1 FROM ProposalAdequacyReview WHERE ProposalAdequacyReviewID = @newChecklistId)
-	BEGIN
-		UPDATE ProposalAdequacyReview SET IsCurrent = 0;
-
-		SET IDENTITY_INSERT ProposalAdequacyReview ON;
-		INSERT INTO ProposalAdequacyReview (ProposalAdequacyReviewID, ChecklistVersion, IsCurrent, ProposalChecklistTypeID)
-			VALUES (@newChecklistId, @newChecklistId - 1, 1, 1);
-		SET IDENTITY_INSERT ProposalAdequacyReview OFF;
-
-		INSERT INTO PARChecklistContent (ChecklistText, TextTypeID, SortOrder, ColumnOrder, ProposalAdequacyReviewID, QuestionNumber, Reference, SubmissionItem, YesOnly)
-			SELECT ChecklistText, TextTypeID, SortOrder, ColumnOrder, @newChecklistId, QuestionNumber, Reference, SubmissionItem, YesOnly
-				FROM PARChecklistContent
-				WHERE ProposalAdequacyReviewId = @newChecklistId - 1;
-	END
-END
-GO
-
-IF  EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CopyCannedResponsesPAR]') AND type in (N'P', N'PC'))
-	DROP PROCEDURE [dbo].[CopyCannedResponsesPAR];
-GO
-
-CREATE PROCEDURE dbo.CopyCannedResponsesPAR
-/******************************************************************************
-**		 
-**		Name: CopyCannedResponsesPAR
-**		Desc: Copies Canned Responses into a new Checklist version. This is executed manually.
-**
-**		Auth: Dusan
-**		Date: 4/13/21
-*******************************************************************************
-**		Change History
-*******************************************************************************
-**		Date:		Author:				Description:
-**		--------	--------			---------------------------------------
-*******************************************************************************/
-(@newChecklistId AS INT) AS
-BEGIN
-	IF NOT EXISTS (SELECT 1 FROM dbo.CannedResponsesPAR cR INNER JOIN PARChecklistContent pCC ON pCC.PARChecklistContentID = cR.QuestionId WHERE pCC.ProposalAdequacyReviewID = @newChecklistId)
-	BEGIN
-		DECLARE @oldChecklistId INT = @newChecklistId - 1;
-
-		INSERT INTO dbo.CannedResponsesPAR
-		SELECT cR.Text, new.PARChecklistContentID
-			FROM dbo.CannedResponsesPAR cR 
-				INNER JOIN PARChecklistContent old ON old.PARChecklistContentID = cR.QuestionId AND old.ProposalAdequacyReviewID = @oldChecklistId
-				INNER JOIN PARChecklistContent new ON old.QuestionNumber = new.QuestionNumber AND new.ProposalAdequacyReviewID = @newChecklistId;
-
-	END
-END
-GO
-
-IF  EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CopyPPRChecklist]') AND type in (N'P', N'PC'))
-	DROP PROCEDURE [dbo].[CopyPPRChecklist];
-GO
-
-CREATE PROCEDURE dbo.CopyPPRChecklist
-(@newChecklistId AS INT) AS
-/******************************************************************************
-**		 
-**		Name: CopyPPRChecklist
-**		Desc: Copies PPR Checklist into a new Checklist version. This is executed manually.
-**
-**		Auth: RJ
-**		Date: 4/13/23
-*******************************************************************************
-**		Change History
-*******************************************************************************
-**		Date:		Author:				Description:
-**		--------	--------			---------------------------------------
-*******************************************************************************/
-BEGIN
-	IF NOT EXISTS (SELECT 1 FROM ProposalPricingReview WHERE ProposalPricingReviewID = @newChecklistId)
-	BEGIN
-		UPDATE ProposalPricingReview SET IsCurrent = 0;
-
-		SET IDENTITY_INSERT ProposalPricingReview ON;
-		INSERT INTO ProposalPricingReview (ProposalPricingReviewID, ChecklistVersion, IsCurrent, ProposalChecklistTypeID)
-			VALUES (@newChecklistId, @newChecklistId - 1, 1, 1);
-		SET IDENTITY_INSERT ProposalPricingReview OFF;
-
-		INSERT INTO PPRChecklistContent (ChecklistText, TextTypeID, SortOrder, ColumnOrder, ProposalPricingReviewID)
-			SELECT ChecklistText, TextTypeID, SortOrder, ColumnOrder, @newChecklistId
-				FROM PPRChecklistContent
-				WHERE ProposalPricingReviewID = @newChecklistId - 1;
-	END
-END
-GO
 
 -- New Checklist version 16, ID 17
 DECLARE @newChecklistId INT = 17;
