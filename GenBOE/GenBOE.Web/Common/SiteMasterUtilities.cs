c0c4da7b63ebf2ab3bf6e746fbdffd93bc0ed94b
Hazrat Rafiqzadah <hazrat.b.rafiqzadah@lmco.com>
2024-02-26 19:52:25 -0500

    Fix naming

--- GenBOE/GenBOE.Web/Common/SiteMasterUtilities.cs
+++ GenBOE/GenBOE.Web/Common/SiteMasterUtilities.cs
@@ -319,12 +319,12 @@ namespace GenBOE.Web.Common
 				{
 					if (!isBrc)
 					{
-						resourceData = resourceData.Where(x => x.ElementOfCost != ElementOfCostType.LMLabor || (x.SegRegion != WebConstants.RMX_1LMX_CORE && x.SegRegion != WebConstants.RMX_1LMX_SERVICES)).ToList();
+						resourceData = resourceData.Where(x => x.ElementOfCost != ElementOfCostType.LMLabor || (x.SegRegion != WebConstants.RMS_1LMX_CORE && x.SegRegion != WebConstants.RMS_1LMX_SERVICES)).ToList();
 
 					}
 					else
 					{
-						resourceData = resourceData.Where(x => x.ElementOfCost != ElementOfCostType.LMLabor || x.SegRegion == WebConstants.RMX_1LMX_CORE || x.SegRegion == WebConstants.RMX_1LMX_SERVICES).ToList();
+						resourceData = resourceData.Where(x => x.ElementOfCost != ElementOfCostType.LMLabor || x.SegRegion == WebConstants.RMS_1LMX_CORE || x.SegRegion == WebConstants.RMS_1LMX_SERVICES).ToList();
 					}
 				}
 			}
