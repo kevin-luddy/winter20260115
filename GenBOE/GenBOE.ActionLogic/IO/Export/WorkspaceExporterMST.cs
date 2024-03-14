80e92ae5a27e0ad8a52320e87c34c0042c539eaa
Breanne <breanne.m.nowicki@lmco.com>
2024-03-13 22:04:36 -0600

    fixed template focus and A19

--- GenBOE/GenBOE.ActionLogic/IO/Export/WorkspaceExporterMST.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/WorkspaceExporterMST.cs
@@ -109,11 +109,8 @@ namespace GenBOE.ActionLogic.IO.Export
             toReturn.Add("Proposal Comments", workspace.StatusComment);
             toReturn.Add("MOQ Template BOEs", workspace.UsingTemplateBOE ? this.sYes : this.sNo);
             toReturn.Add("SAP Connection Enabled", workspace.EnableSAPConnection ?  this.sYes : this.sNo);
-            // For some unknown reason these are needed so the file won't corrupt
-            toReturn.Add(this.sEmpty, this.sEmpty);
-            toReturn.Add(this.sEmpty, this.sEmpty);
 
-            return toReturn;
+	     return toReturn;
         }
 
         /// <summary>
