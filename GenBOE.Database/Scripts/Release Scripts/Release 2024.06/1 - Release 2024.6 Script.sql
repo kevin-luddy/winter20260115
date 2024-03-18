51374ecbb63791d26bd6fc3e3d1dd0956f3be444
Wilson, Timothy <timothy.wilson@lmco.com>
2024-03-18 13:20:00 -0400

    Update BRC Ids to use the new Workspace's ResourceList Ids...isntead of linking to the original Workspace's ResourceList Ids

---
+++ GenBOE.Database/Scripts/Release Scripts/Release 2024.06/1 - Release 2024.6 Script.sql
@@ -0,0 +1,12 @@
+﻿/*
+	## START ##
+	3/18/24		twilson3			PROPH-1760 Fix BRC IDs for new WS
+*/
+
+-- Stored procs changed:
+-- copyWorkspace.sql
+
+/*
+   3/18/24		twilson3			PROPH-1760 Fix BRC IDs for new WS
+   ## END ##
+*/
