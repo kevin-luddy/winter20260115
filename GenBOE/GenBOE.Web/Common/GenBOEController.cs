7827701b2a8f9e9e3eba29a6c483ee9631e48b11
paliderd <paliderd@US05CD3035N2Z>
2024-03-27 09:12:02 -0400

    Matomo changes

--- GenBOE/GenBOE.Web/Common/GenBOEController.cs
+++ GenBOE/GenBOE.Web/Common/GenBOEController.cs
@@ -533,7 +533,8 @@ namespace GenBOE.Web.Common
                 ViewData["TimeWhenWorkspaceWasLocked"] = workspace.DateRecalculationStarted;
             }
 
-            ViewData["CurrentUserName"] = currentUser != null ? currentUser.DisplayName : string.Empty;
+			ViewData["CurrentUserName"] = currentUser != null ? currentUser.DisplayName : string.Empty;
+			ViewData["CurrentNtid"] = currentUser != null ? currentUser.Ntid : string.Empty;
 
             bool isAdmin = false;
             bool isSystemAdmin = false;
