2499d051d4ef1e5b954f929e5cc0bb279b5eef8d
Thomas Asuncion <e405721@US05CG21631N1>
2024-04-18 16:24:31 -0400

    remove banner app

--- GenBOE/IES.Common/classes/ConfigurationUtilities.cs
+++ GenBOE/IES.Common/classes/ConfigurationUtilities.cs
@@ -209,17 +209,6 @@ namespace IES.Common
             return string.IsNullOrEmpty(iesUrl) ? string.Empty : iesUrl;
         }
 
-		/// <summary>
-		/// Gets the IES Banner App name
-		/// </summary>
-		public static string IESBannerApp
-		{
-			get
-			{
-				return ConfigurationUtilities.GetAppSetting("IESBannerApp");
-			}
-		}
-
 		#endregion
 	}
 }
