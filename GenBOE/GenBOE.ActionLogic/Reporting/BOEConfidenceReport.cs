d5329b0468a82a15979d4c71c0b3d2200829200f
RJ <ronald.anzalone@lmco.com>
2024-05-09 16:52:14 -0400

    1795 - confidence report class

---
+++ GenBOE/GenBOE.ActionLogic/Reporting/BOEConfidenceReport.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2024 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.ActionLogic.Reporting
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using GenBOE.DataBridge.GenBOE.Dtos.DTOs;
+	using IES.Common.classes;
+
+	/// <summary>
+	/// class containing logic for the BOE Confidence Report
+	/// </summary>
+	public class BOEConfidenceReport
+	{
+		public BOEConfidenceReport()
+		{
+			
+		}
+	}
+}
