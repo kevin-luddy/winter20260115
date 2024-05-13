dc0794aa63c95135f605536e863c7a8a990c506f
RJ <ronald.anzalone@lmco.com>
2024-05-13 17:10:42 -0400

    1795 - added test class

---
+++ GenBOE/GenBOE.Tests/ActionLogic/Reporting/BOEConfidenceReportTest.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2024 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.Tests.ActionLogic.Reporting
+{
+	using GenBOE.ActionLogic.Reporting;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class BOEConfidenceReportTest
+	{
+		/// <summary>
+		/// Get the System Under Test
+		/// </summary>
+		/// <returns>an instance of BOEConfidenceReports</returns>
+		public BOEConfidenceReport GetSUT()
+		{
+			return new BOEConfidenceReport();
+		}
+	}
+}
