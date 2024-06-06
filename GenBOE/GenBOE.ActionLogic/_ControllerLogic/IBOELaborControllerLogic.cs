82b20ce944b1127714979b69040e3c5c422c4815
Lutz <e419085@us.lmco.com>
2024-06-06 01:07:19 -0600

    updates for SAP API skill mix

--- GenBOE/GenBOE.ActionLogic/_ControllerLogic/IBOELaborControllerLogic.cs
+++ GenBOE/GenBOE.ActionLogic/_ControllerLogic/IBOELaborControllerLogic.cs
@@ -357,5 +357,13 @@ namespace GenBOE.ActionLogic
         /// <returns>Validation Response</returns>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
         Task<ICollection<IESResponse<CalculateActualsViewModel>>> CalculateAllActualsSap(ICollection<MoqTableDataModelView> tableData);
+
+		/// <summary>
+		/// Gets Skill Mix Table results for SAP
+		/// </summary>
+		/// <param name="query">The query</param>
+		/// <returns>Validation Response</returns>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
+		Task<ICollection<IESResponse<SkillMixTableViewModel>>> GetSkillMixTableSap(ICollection<SkillMixTableQueryModelView> query);
     }
 }
---
+++ GenBOE/GenBOE.ActionLogic/_ModelView/Task/SkillMixTableQueryModelView.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2024 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.ActionLogic.ModelView
+{
+    /// <summary>
+    /// Used to retrieve skill mix table
+    /// </summary>
+    public class SkillMixTableQueryModelView
+    {
+        /// <summary>
+		/// Resource Id
+		/// </summary>
+		public string ResourceId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Query Filters
+		/// </summary>
+		public string Filters { get; set; }
+    }
+}
