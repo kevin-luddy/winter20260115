d9a80a1be8f20b1a17d0eb32909fbff8a83039d1
Katie <katherine.k.pham@lmco.com>
2024-06-13 14:09:30 -0600

    Updated tests, controller and webconstants

--- GenBOE/GenBOE.Web/Controllers/ReportsController.cs
+++ GenBOE/GenBOE.Web/Controllers/ReportsController.cs
@@ -906,14 +906,14 @@ namespace GenBOE.Web.Controllers
 		}
 
 		/// <summary>
-		/// Export the Confidence Report Results into an Excel file
+		/// Export the Confidence Report into an Excel file
 		/// </summary>
 		/// <param name="workspace">Workspace Shortname</param>
 		/// <param name="id">BOE ID</param>
-		/// <returns>Excel file of the Confidence Report Results</returns>
+		/// <returns>Excel file of the Confidence Report</returns>
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "Inline use, expecting for garbage collection to take care of things.")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "The UI might hang indefinitely, never returning control to the user, unless all exceptions are handled.")]
-		public ActionResult ExportConfidenceReportResults(string workspace, int? boeID)
+		public ActionResult ExportConfidenceReport(string workspace, int? boeID)
 		{
 			// Look at this as an example
 			// public ActionResult Export(string workspace, int reportID, string summarizeByCustomField)
@@ -923,8 +923,8 @@ namespace GenBOE.Web.Controllers
 			try
 			{
 				// Initialize Action
-				Stopwatch sw = this.InitializeAction(this.log, "ExportConfidenceReportResults", boeID == null ? SecurityPage.Reports : SecurityPage.EditBOEHeader, SecurityAuthorization.Read, ws, boeID);
-				this.log.Performance("Exporting Confidence Report Results - ReportsController - Begin", 0);
+				Stopwatch sw = this.InitializeAction(this.log, "ExportConfidenceReport", boeID == null ? SecurityPage.Reports : SecurityPage.EditBOEHeader, SecurityAuthorization.Read, ws, boeID);
+				this.log.Performance("Exporting Confidence Report - ReportsController - Begin", 0);
 
 				ConfidenceReportModelView confidenceReport = boeConfidenceReport.GenerateConfidenceReport(ws, boeID);
 
@@ -943,7 +943,7 @@ namespace GenBOE.Web.Controllers
 					contentType: ExportFileDownloadBase.GetContentType(fileName),
 					fileDownloadName: fileName);
 
-				this.FinalizeAction(this.log, WebConstants.ACTION_DISPLAY_BOE_CONFIDENCE_REPORT, sw);
+				this.FinalizeAction(this.log, WebConstants.ACTION_EXPORT_CONFIDENCE_REPORT, sw);
 			}
 			catch (GenValidationException ex)
 			{
