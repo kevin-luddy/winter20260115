1022427eb41a39806b1d7969a2eb7ce0e0c07572
e374897 <e374897@US05CG21630KD>
2024-06-13 00:47:24 -0700

    upsertSkillMix via Table

---
+++ GenBOE.Database/Scripts/Object Scripts/Table Based Processing/SkillMixViaTable.sql
@@ -0,0 +1,86 @@
+﻿IF  EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[upsertSkillMixviaTableParameter]') AND type in (N'P', N'PC'))
+	DROP PROCEDURE [dbo].[upsertSkillMixviaTableParameter];
+GO
+
+CREATE TYPE [dbo].[TT_SkillMix] AS TABLE(
+		[Rationale] varchar(255) NULL
+		,[Included] bit DEFAULT 0
+		,[ProposedHours] decimal(11, 2) NULL
+		,[HistoricalHours] decimal(11, 2) NULL
+		,[BOESkillMix] decimal(5, 2) NULL
+		,[LaborSkillMix] decimal(5, 2) NULL
+		,[ResourceOld] [varchar](max) NULL
+		,[ResourceNew] [varchar](max) NULL
+		,[BOEID] int NULL
+		,[BOETaskElementID] int NULL
+		,[MOQTypeSelectionID] int NULL
+);
+GO
+
+SET ANSI_NULLS OFF
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+CREATE PROCEDURE [dbo].[upsertSkillMixviaTableParameter]
+(
+	@SkillMixTableParameter [dbo].[TT_SkillMix] READONLY
+)
+AS
+/******************************************************************************
+**		 
+**		Name: [upsertSkillMixviaTableParameter]
+**		Desc: Insert/Update data into SkillMix Table
+**			
+**		
+**
+**		Auth: Hyun Dong Lee
+**		Date: 6/2024
+*******************************************************************************
+**		Change History
+*******************************************************************************
+**		Date:		Author:				Description:
+**		--------	--------			-------------------------------------------
+**      06/12/24	e374897				PROPH-2047 Initial upsert logic
+*******************************************************************************/
+BEGIN
+	DECLARE @DistinctMOQTypeSelectionID int
+	SELECT @DistinctMOQTypeSelectionID = MOQTypeSelectionID
+	FROM (
+		SELECT DISTINCT MOQTypeSelectionID
+		FROM @SkillMixTableParameter
+	) AS temp_SkillMix
+
+	DELETE FROM [dbo].[SkillMix]
+	WHERE [MOQTypeSelectionID] = @DistinctMOQTypeSelectionID
+
+	INSERT INTO [dbo].[SkillMix]
+		([Rationale]
+		 ,[Included]
+		 ,[ProposedHours]
+		 ,[HistoricalHours]
+		 ,[BOESkillMix]
+		 ,[LaborSkillMix]
+		 ,[ResourceOld]
+		 ,[ResourceNew]
+		 ,[BOEID]
+		 ,[BOETaskElementID]
+		 ,[MOQTypeSelectionID]
+		 )
+	SELECT Rationale
+		,Included
+		,ProposedHours
+		,HistoricalHours
+		,BOESkillMix
+		,LaborSkillMix
+		,ResourceOld
+		,ResourceNew
+		,BOEID
+		,BOETaskElementID
+		,MOQTypeSelectionID
+	FROM @SkillMixTableParameter
+END
+
+IF @@ERROR = 0
+	SELECT COUNT(*) FROM @SkillMixTableParameter
+GO
