44586108fc3736692ab3528580de289df947c23f
e374897 <e374897@US05CG21630KD>
2024-06-13 15:37:59 -0700

    Update GenBOE Models edmx files

--- GenBOE/GenBOE.Models/MOQTypeSelection.cs
+++ GenBOE/GenBOE.Models/MOQTypeSelection.cs
@@ -18,6 +18,7 @@ namespace GenBOE.Models
         public MOQTypeSelection()
         {
             this.MOQTypeSelectionTableDatas = new HashSet<MOQTypeSelectionTableData>();
+            this.SkillMixes = new HashSet<SkillMix>();
         }
     
         public int MOQTypeSelectionId { get; set; }
@@ -39,5 +40,7 @@ namespace GenBOE.Models
         public virtual MOQTypeLU MOQTypeLU { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<MOQTypeSelectionTableData> MOQTypeSelectionTableDatas { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SkillMix> SkillMixes { get; set; }
     }
 }
