6efb90a2a0bc4417aa9aef493c59c697935245e5
e374897 <e374897@US05CG21630KD>
2024-06-17 13:52:07 -0700

    Init SkillMix table

---
+++ GenBOE/GenBOE.ModelView/DTO/SkillMix/ISkillMixDTOLoader.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2024 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.DataBridge.DTO
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class ISkillMixDTOLoader
+	{
+	}
+}
--- GenBOE/GenBOE.ModelView/GenBOE.Dtos/DTOs/SkillMixDTO.cs
+++ GenBOE/GenBOE.ModelView/GenBOE.Dtos/DTOs/SkillMixDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace GenBOE.DataBridge.DTO.SkillMix
+﻿namespace GenBOE.Dtos
 {
 	using System;
 	using System.Collections.Generic;
@@ -6,7 +6,7 @@
 	using System.Text;
 	using System.Threading.Tasks;
 
-	internal class ISkillMixLoader
+	internal class SkillMixDTO
 	{
 	}
 }
