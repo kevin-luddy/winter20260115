ecc4de27f9aa150c48f2898f2917b5fb3109d774
e374897 <e374897@US05CG21630KD>
2024-06-17 12:40:50 -0700

    init skill mix loaders

---
+++ GenBOE/GenBOE.ModelView/DTO/SkillMix/ISkillMixLoader.cs
@@ -0,0 +1,12 @@
+﻿namespace GenBOE.DataBridge.DTO.SkillMix
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	internal class ISkillMixLoader
+	{
+	}
+}
