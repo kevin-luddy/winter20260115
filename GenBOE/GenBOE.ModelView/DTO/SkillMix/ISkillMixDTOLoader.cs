bdcd6c0b92db9191820bc5cea3b9d20e1f14947a
e374897 <e374897@US05CG21630KD>
2024-06-17 14:43:20 -0700

    Set the ISkillMixDTOLoader

--- GenBOE/GenBOE.ModelView/DTO/SkillMix/ISkillMixDTOLoader.cs
+++ GenBOE/GenBOE.ModelView/DTO/SkillMix/ISkillMixDTOLoader.cs
@@ -6,13 +6,48 @@
 
 namespace GenBOE.DataBridge.DTO
 {
+	using GenBOE.Dtos;
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
 
-	public class ISkillMixDTOLoader
+	public interface ISkillMixDTOLoader
 	{
+		/// <summary>
+		/// Get all SkillMix values by MOQTypeSelection FK ID
+		/// </summary>
+		/// <param name="moqTypeSelectionID"></param>
+		/// <returns></returns>
+		ICollection<SkillMixDTO> GetByMOQTypeSelectionID(int moqTypeSelectionID);
+
+		/// <summary>
+		/// Get all SkillMix values by BOE FK ID
+		/// </summary>
+		/// <param name="boeID"></param>
+		/// <returns></returns>
+		ICollection<SkillMixDTO> GetByBOEID(int boeID);
+
+		/// <summary>
+		/// Get all SkillMix values by BOETaskElement FK ID
+		/// </summary>
+		/// <param name="boeTaskElementID"></param>
+		/// <returns></returns>
+		ICollection<SkillMixDTO> GetByBOETaskElementID(int boeTaskElementID);
+
+		/// <summary>
+		/// Get SkillMix by certain values
+		/// </summary>
+		/// <param name="skillMixIDs"></param>
+		/// <returns></returns>
+		ICollection<SkillMixDTO> GetByIds(ICollection<int> skillMixIDs);
+
+		/// <summary>
+		/// Get SkillMix value by a specific primary key
+		/// </summary>
+		/// <param name="skillMixID"></param>
+		/// <returns></returns>
+		SkillMixDTO GetById(int skillMixID);
 	}
 }
