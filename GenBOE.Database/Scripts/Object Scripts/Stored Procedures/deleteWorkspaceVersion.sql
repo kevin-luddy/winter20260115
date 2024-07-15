c1ead27f9c3d9f02f0d2132e4f999feaecd35009
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2024-07-15 11:01:25 -0400

    Reorder delete to prevent fk disconnected records

--- GenBOE.Database/Scripts/Object Scripts/Stored Procedures/deleteWorkspaceVersion.sql
+++ GenBOE.Database/Scripts/Object Scripts/Stored Procedures/deleteWorkspaceVersion.sql
@@ -79,12 +79,12 @@ BEGIN
 	DELETE FROM [version].[CustomField] WHERE VersionID = @VersionID
 	DELETE FROM [version].[CustomFieldValue] WHERE VersionID = @VersionID
 	DELETE FROM [version].[MaterialTaskElement] WHERE VersionID = @VersionID
-	DELETE FROM [version].[MOQTypeSelection] WHERE VersionID = @VersionID
-	DELETE FROM [version].[MoqTypeTableCustomFieldValueXREF] WHERE VersionID = @VersionID
-	DELETE FROM [version].[MOQTypeSelectionTableData] WHERE VersionID = @VersionID
-	DELETE FROM [version].[SkillMix] WHERE VersionID = @VersionID
-	DELETE FROM [version].[CommonDisclosureSkillMix] WHERE VersionID = @VersionID
 	DELETE FROM [version].[MOQTypeSelectionTableDataResourceHours] WHERE VersionID = @VersionID
+	DELETE FROM [version].[CommonDisclosureSkillMix] WHERE VersionID = @VersionID
+	DELETE FROM [version].[SkillMix] WHERE VersionID = @VersionID
+	DELETE FROM [version].[MOQTypeSelectionTableData] WHERE VersionID = @VersionID
+	DELETE FROM [version].[MoqTypeTableCustomFieldValueXREF] WHERE VersionID = @VersionID
+	DELETE FROM [version].[MOQTypeSelection] WHERE VersionID = @VersionID
 	DELETE FROM [version].[ODCSpread] WHERE VersionID = @VersionID
 	DELETE FROM [version].[ODCTaskElement] WHERE VersionID = @VersionID
 	DELETE FROM [version].[ODCType] WHERE VersionID = @VersionID
