ec5e3043616d38ea7fa2eae20328663a0e5df68c
Wilson, Timothy <timothy.wilson@lmco.com>
2024-09-05 18:26:34 -0400

    generate tasks based off the split resource types....so we make sure that the resource is linked correctly

--- GenBOE/GenBOE.ActionLogic/IO/Export/ProPricerExporter.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/ProPricerExporter.cs
@@ -447,7 +447,7 @@ namespace GenBOE.ActionLogic.IO.Export
 						}
 
 						IDictionary<int, string> laborTypeIdToProPricerIdMappings = this.GenerateTaskRow(wsLevelData, inputsForExport, inputsForExport.Clin, inputsForExport.Wbs,
-							elementOfCost, taskResourcesEntriesForElementOfCost, boeTask, taskResourcesEntriesForElementOfCost.Any(x => x.IsOffloaded), workspaceShortname);
+							elementOfCost, resourcesSplitforBrc, boeTask, taskResourcesEntriesForElementOfCost.Any(x => x.IsOffloaded), workspaceShortname);
 
 						foreach (ResourceTypeDto labor in resourcesSplitforBrc)
 						{
@@ -476,7 +476,7 @@ namespace GenBOE.ActionLogic.IO.Export
 						resourcesSplitforBrc = BRCValidationUtility.ProcessLaborTypesForBrc(taskResourcesEntriesForElementOfCost, workspaceShortname, 0).ToList();
 					}
 					IDictionary<int, string> laborTypeIdToProPricerIdMappings = new Dictionary<int, string>();
-					foreach (ResourceTypeDto labor in taskResourcesEntriesForElementOfCost)
+					foreach (ResourceTypeDto labor in resourcesSplitforBrc)
 					{
 						// do not export to ProPricer if task doesn't have any total hours or cost or if there no offsets
 						if (boeTask.TotalHours != 0 || boeTask.TotalCost != 0 || boeTask.taskElementLabors.Any(l => l.ValueSpread != 0))
