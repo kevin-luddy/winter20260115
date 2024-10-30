38c8f54be5e92ebc611a1f15799387a04de628ad
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2024-10-30 10:32:49 -0400

    fix comment

--- GenBOE/GenBOE.ModelView/DTO/MOQTypes/MoqTypeSelection.cs
+++ GenBOE/GenBOE.ModelView/DTO/MOQTypes/MoqTypeSelection.cs
@@ -12,7 +12,6 @@ namespace GenBOE.ActionLogic.ModelView
 
 	/// <summary>
 	/// MOQ Type selection, with underlying data
-	/// The view model that drives MoqEquationCtorller.js.
 	/// </summary>
 	[Serializable]
 	public class MoqTypeSelection : UpdateableDTO
