a54f572f3cf80e9305d9a06040d4ce526437489d
Hazrat Rafiqzadah <hazrat.b.rafiqzadah@lmco.com>
2024-11-19 14:47:21 -0500

    Add back validation call for save

--- GenBOE/GenBOE.ActionLogic/_ControllerLogic/BOELaborControllerLogic.cs
+++ GenBOE/GenBOE.ActionLogic/_ControllerLogic/BOELaborControllerLogic.cs
@@ -897,6 +897,16 @@ namespace GenBOE.ActionLogic.ControllerLogic
 				}
 			}
 
+			if (taskElement.SkillMixTable != null && taskElement.SkillMixTable.Any())
+			{
+				validationErrors.AddRange(ActionLogicUtility.ValidateSkillMixTable(taskElement.SkillMixTable, ws.CreationDate).Select(x => new ValidationMessage(x)));
+			}
+
+			if (taskElement.CommonDisclosureTable != null && taskElement.CommonDisclosureTable.Any())
+			{
+				validationErrors.AddRange(ActionLogicUtility.ValidateCommonDisclosureSkillMixTable(taskElement.CommonDisclosureTable, taskElement.SkillMixTable, ws.CreationDate).Select(x => new ValidationMessage(x)));
+			}
+
 			return validationErrors;
 		}
 
