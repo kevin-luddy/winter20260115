ff23afe6784b274b8b445f9da11406f852325718
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2024-11-19 12:26:06 -0500

    replace broken

--- GenBOE/GenBOE.ActionLogic/_ControllerLogic/WorkspaceControllerLogic.cs
+++ GenBOE/GenBOE.ActionLogic/_ControllerLogic/WorkspaceControllerLogic.cs
@@ -1884,7 +1884,7 @@ namespace GenBOE.ActionLogic.ControllerLogic
 		/// <param name="tables">Dictionary of MOQ Tables</param>
 		/// <param name="tableIdToMoqType">Dictionary of MOQ Types keyed by Table Id</param>
 		/// <param name="workspaceCreationDate">Workspace Creation Date</param>
-		/// <param name="isUsingTM">Workspace setting for using T&M</param>
+		/// <param name="isUsingTM"
 		/// <returns></returns>
 		private async Task<List<int>> RecalculateActualsAcrossWorkspace(List<WorkspaceCalculateActualsModelView> result, Dictionary<int, FullBoe> boes, Dictionary<int, string> tasks, Dictionary<int, MoqTableData> tables, Dictionary<int, MoqTypeSelection> tableIdToMoqType, DateTime? workspaceCreationDate, bool isUsingTM)
 		{
@@ -1903,7 +1903,7 @@ namespace GenBOE.ActionLogic.ControllerLogic
 
 			List<int> boesUpdated = new List<int>();
 			// Make one bulk call to SAP
-			if (Utilities.IsSkillMixEnabledForSystem)
+			if (Utilities.ShowSkillMixForWorkspace(workspaceCreationDate, isUsingTM))
 			{
 				ICollection<IESResponse<CalculateActualsWithSkillMixViewModel>> responses = await this.boeLaborControllerLogic.CalculateAllActualsSapWithSkillMix(tableData);
 				foreach (IESResponse<CalculateActualsWithSkillMixViewModel> response in responses)
