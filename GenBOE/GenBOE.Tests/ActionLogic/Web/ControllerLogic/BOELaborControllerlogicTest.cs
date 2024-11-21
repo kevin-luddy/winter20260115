a8557e8f49b2c850d5ea471650b7cc71a540e3af
Hazrat Rafiqzadah <hazrat.b.rafiqzadah@lmco.com>
2024-11-20 19:04:59 -0500

    Fix build

--- GenBOE/GenBOE.Tests/ActionLogic/Web/ControllerLogic/BOELaborControllerlogicTest.cs
+++ GenBOE/GenBOE.Tests/ActionLogic/Web/ControllerLogic/BOELaborControllerlogicTest.cs
@@ -60,8 +60,6 @@ namespace GenBOE.Tests.ActionLogic.Web.ControllerLogic
 		private Mock<ICommonDataMapper> commonDataMapper = null;
 		private Mock<IRteTemplateDataLoader> rteTemplateDataLoader = null;
 		private Mock<IMoqTypeDataLoader> moqTypeDataLoader = null;
-		private Mock<ISkillMixDTOLoader> skillMixDTOLoader = null;
-		private Mock<ICommonDisclosureSMDTODataLoader> commonDisclosureDTOLoader = null;
 		private Mock<IValidateBOE> validateBOE = null;
 		private Mock<IMoqTableExporter> moqTableExporter = null;
 		private Mock<IMoqTableImporter> moqTableImporter = null;
@@ -176,8 +174,6 @@ namespace GenBOE.Tests.ActionLogic.Web.ControllerLogic
 				   commonDataMapper.Object,
 				   rteTemplateDataLoader.Object,
 				   this.moqTypeDataLoader.Object,
-				   this.skillMixDTOLoader.Object,
-				   this.commonDisclosureDTOLoader.Object,
 				   this.validateBOE.Object,
 				   this.moqTableExporter.Object,
 				   this.moqTableImporter.Object,
