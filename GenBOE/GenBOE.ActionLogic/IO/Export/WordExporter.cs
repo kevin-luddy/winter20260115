d75954c31740ac49ade5d0bad62e4e39efd68a31
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2024-11-25 16:58:33 -0500

    revert ws change

--- GenBOE/GenBOE.ActionLogic/IO/Export/WordExporter.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/WordExporter.cs
@@ -829,7 +829,7 @@ namespace GenBOE.ActionLogic.IO.Export
 			if (skillMixTablesContainer != null)
 			{
 				if ((selectedComponents.Contains(BoeCustomReportComponent.SkillMixTables) || !selectedComponents.Any())
-					&& Utilities.ShowSAPForWorkspace(exportInputs.Workspace.CreationDate))
+					&& Utilities.ShowSkillMixForWorkspace(exportInputs.Workspace.CreationDate))
 				{
 					// populate Current/Legacy Skill Mix Table
 					SdtElement currentTableElement = WordUtilities.GetTaggedChildElement(skillMixTablesContainer, BOEExporterConstants.Table_CurrentSkillMix);
