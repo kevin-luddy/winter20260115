4c896965cdd0e5dcd4a4db744c2037f0a0944335
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-01-06 21:18:29 -0500

    update for new controller

---
+++ GenBOE/GenBOE.Web/Controllers/Angular/BOEConfigurationController.cs
@@ -0,0 +1,120 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2021 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.Web.Controllers
+{
+	using GenBOE.DataBridge.Common.Interfaces;
+	using GenBOE.DataBridge.DTO;
+	using GenBOE.Objects;
+	using GenBOE.Web.Common;
+	using GenBOE.Web.ModelView;
+	using IES.Common;
+	using IES.Common.classes;
+	using System;
+	using System.Linq;
+	using System.Web.Http;
+
+	/// <summary>
+	/// BOE Configuration Controller used to communicate with the new front-end of GenBOE.
+	/// This controller is used to facilitate configuraiton data.
+	/// </summary>
+	[AllowAnonymous]
+	public class BOEConfigurationController : BoeDataBaseAPIController
+	{
+		#region Properties & Ctor
+
+		/// <summary>
+		/// Token Handling
+		/// </summary>
+		private readonly TokenHandling tokenHandler;
+
+		/// <summary>
+		/// Logger
+		/// </summary>
+		private Logger logger = new Logger("BOEConfigurationController");
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="loader">Workspace loader</param>
+		/// <param name="tokenHandler">Token handler</param>
+		/// <param name="reportsControllerLogic">Reports controller logic</param>
+		/// <param name="securityAccess">Security Access</param>
+		/// <param name="factory">Full object factory</param>
+		/// <param name="userLoader">User loader</param>
+		/// <param name="permissionsLoader">Permission loader</param>
+		/// <param name="boeExporter">BOE exporter</param>
+		/// <param name="boeCustomExporter">BOE custom exporter</param>
+		/// <param name="workspaceExportFormatDTOLoader">Workspace export format loader</param>
+		/// <param name="traceTableExporter">Trace Table data exporter</param>
+		/// <param name="boeFormControllerLogic">BOE Form Controller logic</param>
+		/// <param name="contractTypeLoader">Pick List loader for Contract Types</param>
+		public BOEConfigurationController(TokenHandling tokenHandler, ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader)
+			: base(securityAccess, factory, userLoader, permissionsLoader)
+		{
+			this.tokenHandler = tokenHandler;
+		}
+		#endregion
+
+		/// <summary>
+		/// Get GenBOE configuration.
+		/// </summary>
+		/// <returns>GenBOE Configuration Data</returns>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public IESResponse<BOEConfigurationData> GetGenBOEConfigurationData()
+		{
+			IESResponse<BOEConfigurationData> result = new IESResponse<BOEConfigurationData>();
+
+			try
+			{
+				tokenHandler.AuthenticateUserFromAuthorizationToken();
+
+				result.Data.FirstOrDefault().CompanyConfiguration = SystemConfiguration.Instance().CompanyMode;
+				result.Data.FirstOrDefault().IsBRCEnabled = ConfigurationUtilities.GetAppSetting<bool>("EnableBRC");
+				result.Data.FirstOrDefault().IsReadOnly = ConfigurationUtilities.GetAppSetting<bool>("IsReadOnly");
+				result.Data.FirstOrDefault().DisableExternalLinksForClassifiedInstall = Utilities.DisableExternalHelpLinksForClassifiedInstallations();
+				result.Data.FirstOrDefault().EnableSendToProPricerDirectly = ConfigurationUtilities.GetAppSetting<bool>("EnableSendToProPricerDirectly", false);
+				result.Data.FirstOrDefault().UnclassifiedBannerText = ConfigurationUtilities.GetAppSetting("UnclassifiedBannerText");
+				result.Data.FirstOrDefault().EnableSAP = ConfigurationUtilities.GetAppSetting<bool>("EnableSAP");
+				result.Data.FirstOrDefault().ShowEquivalentPersonsOption = ConfigurationUtilities.GetAppSetting<bool>("ShowEquivalentPersonsOption");
+				result.Data.FirstOrDefault().IsBOEFormVisible = ConfigurationUtilities.GetAppSetting<bool>("IsBOEFormVisible");
+
+				// Get 'Show IES Header' config value for business areas that aren't RMS (not needed for RMS).
+				result.Data.FirstOrDefault().ShowIESHeader = SystemConfiguration.Instance().CompanyMode != CompanyConfiguration.MST && SiteMasterUtilities.ShowIesHeader;
+
+				result.Data.FirstOrDefault().CanCreateWorkspaceWithoutPtmTrackingNumber = ConfigurationUtilities.GetAppSetting<bool>("CanCreateWorkspaceWithoutPtmTrackingNumber");
+				result.Data.FirstOrDefault().ArchiveUrlBOE = ConfigurationUtilities.GetAppSetting("ArchiveUrlBoe");
+				result.Data.FirstOrDefault().ProductionUrl = ConfigurationUtilities.GetAppSetting("ProdUrlBoe");
+				result.Data.FirstOrDefault().ServerUrl = ConfigurationUtilities.GetAppSetting("ServerURL");
+				result.Data.FirstOrDefault().IsRMSArchive = SiteMasterUtilities.IsRMSArchive();
+				result.Data.FirstOrDefault().IsRMSProduction = SiteMasterUtilities.IsRMSProduction();
+				result.Data.FirstOrDefault().IsProjectMapEnabled = ConfigurationUtilities.GetAppSetting<bool>("IsProjectMapEnabled");
+				result.Data.FirstOrDefault().MaximumRowsInProjectMap = ConfigurationUtilities.GetAppSetting<int>("MaximumRowsInProjectMap");
+				result.Data.FirstOrDefault().ProjectMapPageSize = ConfigurationUtilities.GetAppSetting<int>("ProjectMapPageSize");
+				result.Data.FirstOrDefault().ReportServerLocation = ConfigurationUtilities.GetAppSetting("ReportServerLocation");
+				result.Data.FirstOrDefault().ReportServerFolderName = ConfigurationUtilities.GetAppSetting("ReportServerFolderName");
+				result.Data.FirstOrDefault().ApplicationVersion = ConfigurationUtilities.GetAppSetting("APPLICATION_VERSION");
+				result.Data.FirstOrDefault().DisableAllEmails = ConfigurationUtilities.GetAppSetting<bool>("DisableAllEmails");
+				// TODO Thomas: Warning Banner shown on System/Workspace Email Preferences page?
+				result.Data.FirstOrDefault().EmailsToCurrentlyLoggedInUser = ConfigurationUtilities.GetAppSetting<bool>("EmailsToCurrentlyLoggedInUser");
+				result.Data.FirstOrDefault().ShowUserName = ConfigurationUtilities.GetAppSetting<bool>("ShowUserName");
+				result.Data.FirstOrDefault().IsIdentitySwappingAllowed = ConfigurationUtilities.GetAppSetting<bool>("IsIdentitySwappingAllowed");
+				result.Data.FirstOrDefault().ShowCompanyConfiguration = ConfigurationUtilities.GetAppSetting<bool>("ShowCompanyConfiguration");
+
+				result.IsSuccessful = true;
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+				result.Messages.Add($"Unknown error occurred returning Gen BOE Configuration data: {ex.Message}");
+			}
+
+			return result;
+		}
+
+	}
+}
---
+++ GenBOE/GenBOE.Web/ModelView/BOEConfigurationData.cs
@@ -0,0 +1,153 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2021 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.Web.ModelView
+{
+	using IES.Common;
+
+	/// <summary>
+	/// BOE Configuration Class (data returned from GenBOE's Web.config going to the new GenBOE UI)
+	/// 
+	/// THIS MUST MATCH THE BOE SERVICE, DO NOT CHANGE UNLESS CHANGES ARE MADE TO BOTH CLASSES.
+	/// </summary>
+	public class BOEConfigurationData
+	{
+		/// <summary>
+		/// Company Configuration / Company Mode for the instance.
+		/// </summary>
+		public CompanyConfiguration CompanyConfiguration { get; set; }
+
+		/// <summary>
+		/// Is BRC Enabled for the system configuration?
+		/// </summary>
+		public bool IsBRCEnabled { get; set; }
+
+		/// <summary>
+		/// Is the entire site in Read Only mode?
+		/// </summary>
+		public bool IsReadOnly { get; set; }
+
+		/// <summary>
+		/// Are external links for classified install disabled for the entire system configuration?
+		/// </summary>
+		public bool DisableExternalLinksForClassifiedInstall { get; set; }
+
+		/// <summary>
+		/// Is sending to Pro Pricer directly enabled for the system configuration?
+		/// </summary>
+		public bool EnableSendToProPricerDirectly { get; set; }
+
+		/// <summary>
+		/// The text for the Unclassified Banner.
+		/// </summary>
+		public string UnclassifiedBannerText { get; set; }
+
+		/// <summary>
+		/// Is SAP enabled from the system configuration?
+		/// </summary>
+		public bool EnableSAP { get; set; }
+
+		/// <summary>
+		/// Is the option to show equivalent persons enabled?
+		/// </summary>
+		public bool ShowEquivalentPersonsOption { get; set; }
+
+		/// <summary>
+		/// Is the BOE Form Visible
+		/// </summary>
+		public bool IsBOEFormVisible { get; set; }
+
+		/// <summary>
+		/// Show the IES Header?
+		/// </summary>
+		public bool ShowIESHeader { get; set; }
+
+		/// <summary>
+		/// Can create workspace without PTM Tracking Number?
+		/// </summary>
+		public bool CanCreateWorkspaceWithoutPtmTrackingNumber { get; set; }
+
+		/// <summary>
+		/// The Archive Url BOE configuration value.
+		/// </summary>
+		public string ArchiveUrlBOE { get; set; }
+
+		/// <summary>
+		/// The production url from the configuration.
+		/// </summary>
+		public string ProductionUrl { get; set; }
+
+		/// <summary>
+		/// The server url from the configuration.
+		/// </summary>
+		public string ServerUrl { get; set; }
+
+		/// <summary>
+		/// Is instance RMS Archive from configuration?
+		/// </summary>
+		public bool IsRMSArchive { get; set; }
+
+		/// <summary>
+		/// Is instance RMS Production from configuration?
+		/// </summary>
+		public bool IsRMSProduction { get; set; }
+
+		/// <summary>
+		/// Is Project Map Enabled from configuration?
+		/// </summary>
+		public bool IsProjectMapEnabled { get; set; }
+
+		/// <summary>
+		/// The max amount of rows in project map from configuration.
+		/// </summary>
+		public int MaximumRowsInProjectMap { get; set; }
+
+		/// <summary>
+		/// The page size for the project map.
+		/// </summary>
+		public int ProjectMapPageSize { get; set; }
+
+		/// <summary>
+		/// The report server location.
+		/// </summary>
+		public string ReportServerLocation {  get; set; }
+
+		/// <summary>
+		/// The report server folder name.
+		/// </summary>
+		public string ReportServerFolderName { get; set; }
+
+		/// <summary>
+		/// The application version.
+		/// </summary>
+		public string ApplicationVersion { get; set; }
+
+		/// <summary>
+		/// Option to disable all emails.
+		/// </summary>
+		public bool DisableAllEmails { get; set; }
+
+		/// <summary>
+		/// Option to allow emails to currently logged in user.
+		/// </summary>
+		public bool EmailsToCurrentlyLoggedInUser { get; set; }
+
+		/// <summary>
+		/// Option to show user name.
+		/// </summary>
+		public bool ShowUserName { get; set; }
+
+		/// <summary>
+		/// Option to allow identity swaping.
+		/// </summary>
+		public bool IsIdentitySwappingAllowed { get; set; }
+
+		/// <summary>
+		/// Option to show company configuration (company mode).
+		/// </summary>
+		public bool ShowCompanyConfiguration { get; set; }
+	}
+}
