0f54e7ea4063d337cb11b7909f7561925cef6534
Wilson, Timothy <timothy.wilson@lmco.com>
2025-01-14 16:08:21 -0500

    Initial file for UCOT updates

---
+++ GenBOE.Database/Scripts/Release Scripts/Release 2024.14/1 - Release 2024.14 Script.sql
@@ -0,0 +1,17 @@
+﻿EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2024.14'; 
+GO
+
+
+/*
+	## START ##
+
+	1/14/2025 [twilson3] - SLMX_POLM_PROPH-2596 - UCOT database updates
+*/
+
+
+
+/*
+	1/14/2025 [twilson3] - SLMX_POLM_PROPH-2596 - UCOT database updates
+
+	## END ##
+*/
