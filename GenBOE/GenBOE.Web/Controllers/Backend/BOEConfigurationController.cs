05b2b2d219ee8c4b1c9209267c22a218940aef02
e374897 <e374897@US05CG21630KD>
2025-01-20 13:53:26 -0800

    Get HomeMasterMenu

--- GenBOE/GenBOE.Web/Controllers/Backend/BOEConfigurationController.cs
+++ GenBOE/GenBOE.Web/Controllers/Backend/BOEConfigurationController.cs
@@ -6,6 +6,12 @@
 
 namespace GenBOE.Web.Controllers
 {
+	using System;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Web.Http;
+	using GenBOE.ActionLogic;
+	using GenBOE.ActionLogic.ModelView;
 	using GenBOE.DataBridge.Common.Interfaces;
 	using GenBOE.DataBridge.DTO;
 	using GenBOE.Objects;
@@ -13,8 +19,6 @@ namespace GenBOE.Web.Controllers
 	using GenBOE.Web.ModelView;
 	using IES.Common;
 	using IES.Common.classes;
-	using System;
-	using System.Web.Http;
 
 	/// <summary>
 	/// BOE Configuration Controller used to communicate with the new front-end of GenBOE.
@@ -31,6 +35,11 @@ namespace GenBOE.Web.Controllers
 		private Logger logger = new Logger("BOEConfigurationController");
 
 		/// <summary>
+		/// HomeController Logic
+		/// </summary>
+		protected IHomeControllerLogic homeControllerLogic { get; set; }
+
+		/// <summary>
 		/// Ctor
 		/// </summary>
 		/// <param name="tokenHandler">Token handler</param>
@@ -38,11 +47,12 @@ namespace GenBOE.Web.Controllers
 		/// <param name="factory">Full object factory</param>
 		/// <param name="userLoader">User loader</param>
 		/// <param name="permissionsLoader">Permission loader</param>
+		/// <param name="homeControllerLogic">Home Controller Logic</param>
 
-		public BOEConfigurationController(ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader)
+		public BOEConfigurationController(ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader, IHomeControllerLogic homeControllerLogic)
 			: base(securityAccess, factory, userLoader, permissionsLoader)
 		{
-
+			this.homeControllerLogic = homeControllerLogic;
 		}
 		#endregion
 
@@ -142,6 +152,85 @@ namespace GenBOE.Web.Controllers
 			{
 				logger.Error(ex);
 				result.Messages.Add($"Unknown error occurred returning Workspace data: {ex.Message}");
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Returns the menu items for HomePageMenu
+		/// </summary>
+		/// <returns>genBOE Home Master Menu</returns>
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public IESResponse<GenBOEMasterMenuItemModelView> GetHomeMasterMenuItems()
+		{
+			IESResponse<GenBOEMasterMenuItemModelView> result = new IESResponse<GenBOEMasterMenuItemModelView>();
+
+			Collection<GenBOEMasterMenuItemModelView> MenuItems = GenBOEMasterMenuItemModelView.BuildHomeMasterMenuItems();
+
+			GenBOEMasterMenuItemModelView AdminMenuItem = MenuItems.Where(x => x.linkText == "Admin").Single().subMenuItems.Where(y => y.linkText == "Metrics Administration").SingleOrDefault();
+
+			if (AdminMenuItem != null)
+			{
+				homeControllerLogic.PopulateMetricsAdminCompanySpecificProperties(AdminMenuItem);
+			}
+
+			if (Utilities.DisableExternalHelpLinksForClassifiedInstallations())
+			{
+				foreach (GenBOEMasterMenuItemModelView item in MenuItems.ToList())
+				{
+					if (item.securityPage == SecurityPage.AboutToolsMenu
+						|| item.securityPage == SecurityPage.HelpMenu
+						|| item.securityPage == SecurityPage.ContactMenu)
+					{
+						MenuItems.Remove(item);
+					}
+				}
+			}
+
+			foreach (GenBOEMasterMenuItemModelView menuItem in MenuItems) 
+			{
+				// For menu items with no sub items, check access
+				if (menuItem.subMenuItems.Count == 0)
+				{
+					// If the user has access to this Security Page (which has a given server action), add the menu item to the Model Views
+					// OR if the user has access to a URL link, add the menu item
+					if ((menuItem.actionName.Length > 0 && CheckPermission(menuItem.securityPage, null) != SecurityAuthorization.None) ||
+					   (menuItem.linkUrl != null && CheckPermission(menuItem.securityPage, null) != SecurityAuthorization.None))
+					{
+						result.Data.Add(menuItem);
+					}
+				}
+				// If the menu items has sub items, we'll check access on each
+				else
+				{
+					// Create a new model View for the inactive top-level menu item
+					GenBOEMasterMenuItemModelView inactiveMenuItem = new GenBOEMasterMenuItemModelView
+					{
+						linkText = menuItem.linkText
+					};
+
+					// Iterate the sub items
+					foreach (GenBOEMasterMenuItemModelView subMenuItem in menuItem.subMenuItems)
+					{
+						// If the user has access to this Security Page (which has a given server action), add the sub menu item to the
+						// inactive top-level menu item Model View
+						// OR if the user has access to a URL link, add the menu item
+						if ((subMenuItem.actionName.Length > 0 && CheckPermission(subMenuItem.securityPage, null) != SecurityAuthorization.None) ||
+							(subMenuItem.linkUrl != null && CheckPermission(subMenuItem.securityPage, null) != SecurityAuthorization.None))
+						{
+							inactiveMenuItem.subMenuItems.Add(subMenuItem);
+						}
+					}
+
+					// If the inactive top-level menu item has sub items that the user has access to,
+					// we'll add the top-level menu item to the Model Views
+					if (inactiveMenuItem.subMenuItems.Count > 0)
+					{
+						result.Data.Add(inactiveMenuItem);
+					}
+				}
 			}
 
 			return result;
--- GenBOE/GenBOE.Web/Controllers/BoeDataBaseAPIController.cs
+++ GenBOE/GenBOE.Web/Controllers/BoeDataBaseAPIController.cs
@@ -22,109 +22,109 @@ namespace GenBOE.Web.Controllers
 	/// Base BOE Api data controller
 	/// </summary>
 	public abstract class BoeDataBaseAPIController : ApiController
-    {
-        /// <summary>
-        /// Security Access
-        /// </summary>
-        protected ISecurityAccess SecurityAccess { get; set; }
+	{
+		/// <summary>
+		/// Security Access
+		/// </summary>
+		protected ISecurityAccess SecurityAccess { get; set; }
 
-        /// <summary>
-        /// Full object factory
-        /// </summary>
-        protected IFullObjectFactory Factory { get; set; }
+		/// <summary>
+		/// Full object factory
+		/// </summary>
+		protected IFullObjectFactory Factory { get; set; }
 
-        /// <summary>
-        /// User loader
-        /// </summary>
-        protected IUserDTODataLoader UserLoader { get; set; }
+		/// <summary>
+		/// User loader
+		/// </summary>
+		protected IUserDTODataLoader UserLoader { get; set; }
 
-        /// <summary>
-        /// Permission loader
-        /// </summary>
-        protected IPermissionsDTODataLoader PermissionsLoader { get; set; }
+		/// <summary>
+		/// Permission loader
+		/// </summary>
+		protected IPermissionsDTODataLoader PermissionsLoader { get; set; }
 
-        /// <summary>
-        /// Ctor
-        /// </summary>
-        /// <param name="securityAccess">Security access</param>
-        /// <param name="factory">Full object factory</param>
-        /// <param name="userLoader">User loader</param>
-        /// <param name="permissionsLoader">Permission loader</param>
-        protected BoeDataBaseAPIController(ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader)
-        {
-            this.Factory = factory;
-            this.UserLoader = userLoader;
-            this.PermissionsLoader = permissionsLoader;
-            this.SecurityAccess = securityAccess;
-        }
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="securityAccess">Security access</param>
+		/// <param name="factory">Full object factory</param>
+		/// <param name="userLoader">User loader</param>
+		/// <param name="permissionsLoader">Permission loader</param>
+		protected BoeDataBaseAPIController(ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader)
+		{
+			this.Factory = factory;
+			this.UserLoader = userLoader;
+			this.PermissionsLoader = permissionsLoader;
+			this.SecurityAccess = securityAccess;
+		}
 
-        /// <summary>
-        /// Checks if user has permission to Security Page inside a Workspace.
-        /// First, checks whether workspace has OCI.  If true, then checks user's permission to workspace; otherwise, returns true.
-        /// </summary>
-        /// <param name="page">The security page to check for access</param>
-        /// <param name="workspace">The workspace to check for access</param>
-        /// <returns>True if user has permission to Security Page inside a Workspace.</returns>
-        protected bool HasOciPermission(SecurityPage page, WorkspaceDTO workspace)
-        {
-            if (workspace == null)
-            {
-                throw new ArgumentNullException(nameof(workspace));
-            }
+		/// <summary>
+		/// Checks if user has permission to Security Page inside a Workspace.
+		/// First, checks whether workspace has OCI.  If true, then checks user's permission to workspace; otherwise, returns true.
+		/// </summary>
+		/// <param name="page">The security page to check for access</param>
+		/// <param name="workspace">The workspace to check for access</param>
+		/// <returns>True if user has permission to Security Page inside a Workspace.</returns>
+		protected bool HasOciPermission(SecurityPage page, WorkspaceDTO workspace)
+		{
+			if (workspace == null)
+			{
+				throw new ArgumentNullException(nameof(workspace));
+			}
 
-            bool permissionOk = true;
-            if (workspace.ContainsOCI)
-            {
-                SecurityAuthorization permission = this.CheckPermission(page, workspace);
+			bool permissionOk = true;
+			if (workspace.ContainsOCI)
+			{
+				SecurityAuthorization permission = this.CheckPermission(page, workspace);
 
-                if (permission < SecurityAuthorization.Read)
-                {
-                    permissionOk = false;
-                }
-            }
+				if (permission < SecurityAuthorization.Read)
+				{
+					permissionOk = false;
+				}
+			}
 
-            return permissionOk;
-        }
+			return permissionOk;
+		}
 
-        /// <summary>
-        /// Checks permission for page (Some logic pulled from GenBOEController.cs)
-        /// </summary>
-        /// <param name="page">Security Page</param>
-        /// <param name="workspace">Workspace</param>
-        /// <returns>Security Authorization</returns>
-        protected SecurityAuthorization CheckPermission(SecurityPage page, WorkspaceDTO workspace)
-        {
-            Dictionary<SecurityPage, SecurityAuthorization> securityDictionary = new Dictionary<SecurityPage, SecurityAuthorization>();
-            int? wsId = workspace == null ? null : (int?)workspace.Id;
+		/// <summary>
+		/// Checks permission for page (Some logic pulled from GenBOEController.cs)
+		/// </summary>
+		/// <param name="page">Security Page</param>
+		/// <param name="workspace">Workspace</param>
+		/// <returns>Security Authorization</returns>
+		protected SecurityAuthorization CheckPermission(SecurityPage page, WorkspaceDTO workspace)
+		{
+			Dictionary<SecurityPage, SecurityAuthorization> securityDictionary = new Dictionary<SecurityPage, SecurityAuthorization>();
+			int? wsId = workspace == null ? null : (int?)workspace.Id;
 
-            UserDTO user = this.UserLoader.GetUserForActiveUser();
-            string overrideNonUsString = ConfigurationUtilities.GetAppSetting("OverrideSubNonUs");
-            bool overrideNonUs = string.IsNullOrEmpty(overrideNonUsString) ? false : overrideNonUsString.ToLower() == "true";
-            bool? isUsPerson = overrideNonUs ? true : user.IsUsPerson;
+			UserDTO user = this.UserLoader.GetUserForActiveUser();
+			string overrideNonUsString = ConfigurationUtilities.GetAppSetting("OverrideSubNonUs");
+			bool overrideNonUs = string.IsNullOrEmpty(overrideNonUsString) ? false : overrideNonUsString.ToLower() == "true";
+			bool? isUsPerson = overrideNonUs ? true : user.IsUsPerson;
 
-            if (isUsPerson == null)
-            {
-                throw new ValidationException("IsUsPerson cannot be null");
-            }
+			if (isUsPerson == null)
+			{
+				throw new ValidationException("IsUsPerson cannot be null");
+			}
 
-            IReadOnlyCollection<SecurityPermissionsResponse> rolesForUser = this.Factory.GetPermissionsForUser(user.NTID);
+			IReadOnlyCollection<SecurityPermissionsResponse> rolesForUser = this.Factory.GetPermissionsForUser(user.NTID);
 
-            SecurityAuthorization authorizationForUser;
+			SecurityAuthorization authorizationForUser;
 
-            if ((bool)isUsPerson)
-            {
-                authorizationForUser = SecurityAccess.IsAuthorized(
-                    new SecurityPermissionsRequested { PageToCheck = page, WorkspaceId = wsId }, workspace, rolesForUser);
-            }
-            else
-            {
-                throw new UnauthorizedAccessException("Access is denied for non-US users.");
-            }
+			if ((bool)isUsPerson)
+			{
+				authorizationForUser = SecurityAccess.IsAuthorized(
+					new SecurityPermissionsRequested { PageToCheck = page, WorkspaceId = wsId }, workspace, rolesForUser);
+			}
+			else
+			{
+				throw new UnauthorizedAccessException("Access is denied for non-US users.");
+			}
 
-            securityDictionary.Add(page, authorizationForUser);
+			securityDictionary.Add(page, authorizationForUser);
 
-            return securityDictionary.Values.First();
-        }
+			return securityDictionary.Values.First();
+		}
 
-    }
+	}
 }
