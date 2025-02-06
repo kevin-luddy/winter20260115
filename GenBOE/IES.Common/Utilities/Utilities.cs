ef8567ef43574bd028c25f5c966febaaa0d58fe2
Hazrat Rafiqzadah <hazrat.b.rafiqzadah@lmco.com>
2025-02-06 11:11:22 -0500

    Add feature flag for Assign Task Author

--- GenBOE/IES.Common/Utilities/Utilities.cs
+++ GenBOE/IES.Common/Utilities/Utilities.cs
@@ -1039,10 +1039,36 @@ namespace IES.Common
 
 				return isSkillMixEnabled.Value;
 			}
-			internal set // be able to override for unit test purposes
+
+			// be able to override for unit test purposes
+			internal set => isSkillMixEnabled = value;
+		}
+
+		/// <summary>
+		/// Private for Is Task Assign Author Enabled, used for unit testing. Following above design principle
+		/// </summary>
+		private static bool? isAssignTaskAuthorEnabled;
+
+		/// <summary>
+		/// Indicates whether Task Assign Author features are enabled
+		/// </summary>
+		public static bool IsAssignTaskAuthorEnabledForSystem
+		{
+			get
 			{
-				isSkillMixEnabled = value;
+				if (isAssignTaskAuthorEnabled == null)
+				{
+					if (bool.TryParse(ConfigurationUtilities.GetAppSetting("EnableAssignTaskAuthor"), out bool value))
+					{
+						isAssignTaskAuthorEnabled = value;
+					}
+				}
+
+				return isAssignTaskAuthorEnabled ?? false;
 			}
+
+			// be able to override for unit test purposes
+			internal set => isAssignTaskAuthorEnabled = value;
 		}
 
 		/// <summary>
