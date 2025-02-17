1b05f22a1fe131f51f602ad2d65fa3e0c2dadd4b
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-02-17 10:41:09 -0500

    run proc

--- GenBOE/IES.DataBridge.Core/Loaders/DocumentDetailLoader.cs
+++ GenBOE/IES.DataBridge.Core/Loaders/DocumentDetailLoader.cs
@@ -133,7 +133,7 @@ namespace IES.DataBridge.Loaders
             {
                 using (IESEntities context = new())
                 {
-                    result = context.upsertRDSBDocumentInformation(dtoToUpsert.Id, dtoToUpsert.UpdateDate, dtoToUpsert.ProposalId, dtoToUpsert.DocumentCreatedBy, dtoToUpsert.SelectedRevisionId, dtoToUpsert.StartYear, dtoToUpsert.EndYear, dtoToUpsert.ParentSection).First();
+                    result = context.upsertRDSBDocumentInformation(dtoToUpsert.Id, dtoToUpsert.UpdateDate, dtoToUpsert.ProposalId, dtoToUpsert.DocumentCreatedBy, dtoToUpsert.SelectedRevisionId, dtoToUpsert.StartYear, dtoToUpsert.EndYear, dtoToUpsert.ParentSection, (int?)dtoToUpsert.RevisonSegment).First();
                 }
             }
 
