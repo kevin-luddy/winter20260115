713e8acfec9e32c55fd24770041e38591d5628f1
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-03-06 16:14:13 -0500

    update comment

--- GenBOE.Database/Scripts/Object Scripts/Table Based Processing/TaskElementViaTable.sql
+++ GenBOE.Database/Scripts/Object Scripts/Table Based Processing/TaskElementViaTable.sql
@@ -131,6 +131,7 @@ AS
 **		1/16/18		twilson3			BOEJ-2887 Remove Historical Metrics
 **		4/2/18		ranzalon			BOEJ-3268 - Update for Open Ended Custom Fields
 **		6/5/20		ranzalon			BOEJ-4658 - Update for RTE Template Answers
+**		3/6/25		e405721				PROPH-2895 - Update Delete for Skill Mix and Common Disclosure
 *******************************************************************************/
 SET NOCOUNT ON 
 
