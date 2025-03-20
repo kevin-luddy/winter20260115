c369bc6c799c57a5caab76569eced55e6a473fca
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-03-20 11:54:19 -0400

    update total hours

--- GenBOE/GenBOE.ActionLogic/IO/Export/BOE/BOEExportInputs.cs
+++ GenBOE/GenBOE.ActionLogic/IO/Export/BOE/BOEExportInputs.cs
@@ -149,7 +149,7 @@ namespace GenBOE.ActionLogic.IO.Export.BOE
 				foreach (BoeTaskElementDTO taskElement in taskElements)
 				{
 					taskElement.taskElementLabors = taskElementLabors.Where(x => x.TaskElementId == taskElement.Id).ToCollection();
-					taskElement.TotalHours = taskElement.taskElementLabors.Where(tl => tl.SpreadType == SpreadType.Hours).Sum(l => l.ValueSpread);
+					taskElement.TotalHours = taskElementLabors.Where(tl => tl.SpreadType == SpreadType.Hours).Sum(l => l.ValueSpread);
 				}
 			}
 
