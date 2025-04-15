a2f0cb11ec892f05fd19d6b39410cccde7a19c3d
Wilson, Timothy <timothy.wilson@lmco.com>
2025-04-15 12:50:36 -0400

    Updates for 9.5.105.1 dlls

--- ProPricer WebApi/ProPricerWebApi/Pages/Index.cshtml.cs
+++ ProPricer WebApi/ProPricerWebApi/Pages/Index.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace APTSPropricerApi.Pages
 {
+	/// <summary>
+	/// Index Model
+	/// </summary>
 	public class IndexModel : PageModel
 	{
+		/// <summary>
+		/// On Get for homepage
+		/// </summary>
 		public void OnGet()
 		{
 		}
