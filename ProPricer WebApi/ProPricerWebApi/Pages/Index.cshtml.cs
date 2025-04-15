f9b6e55e36240ee31580d7bb2a57555a9922c870
Wilson, Timothy <timothy.wilson@lmco.com>
2025-04-15 12:59:09 -0400

    warnings

--- ProPricer WebApi/ProPricerWebApi/Pages/Index.cshtml.cs
+++ ProPricer WebApi/ProPricerWebApi/Pages/Index.cshtml.cs
@@ -1,8 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace APTSPropricerApi.Pages
 {
+	using Microsoft.AspNetCore.Mvc.RazorPages;
+
 	/// <summary>
 	/// Index Model
 	/// </summary>
