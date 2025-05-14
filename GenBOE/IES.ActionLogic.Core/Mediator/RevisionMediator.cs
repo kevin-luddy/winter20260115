de795750a8f54374deb37730ae44f109a9b6edf2
Yansen Tjandra <yansen.tjandra@lmco.com>
2025-05-14 16:12:18 -0400

    Update Revision GetById to include -1 for new Rate

--- GenBOE/IES.ActionLogic.Core/Mediator/RevisionMediator.cs
+++ GenBOE/IES.ActionLogic.Core/Mediator/RevisionMediator.cs
@@ -51,15 +51,15 @@ namespace IES.ActionLogic.Core.Mediator
 
 		/// <summary>
 		/// Get the specified revision.
-		/// If Id is null, return current WIP revision.
+		/// If Id is null or less than 0, return current WIP revision.
 		/// </summary>
 		/// <param name="id">Revision ID</param>
-		/// <returns>If Id is null, return current WIP revision; othewise, return specified revision.</returns>
+		/// <returns>If Id is null or less than 0, return current WIP revision; othewise, return specified revision.</returns>
 		public RevisionModelView GetById(int? id)
 		{
 			IList<RevisionModelView> revisions = GetAll();
 
-			RevisionModelView revision = id == null ?
+			RevisionModelView revision = (id == null || id < 0) ?
 						revisions.FirstOrDefault(r => r.DatePublished == null) :
 						revisions.FirstOrDefault(r => r.Id == id.Value);
 
