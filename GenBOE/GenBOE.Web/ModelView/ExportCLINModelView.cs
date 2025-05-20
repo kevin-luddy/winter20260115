1a15f3a6f0c863865ca1ce97a0957a80eb3cb0de
e374897 <e374897@US05CG21630KD>
2025-05-19 23:29:51 -0700

    working export

---
+++ GenBOE/GenBOE.Web/ModelView/ExportCLINModelView.cs
@@ -0,0 +1,23 @@
+﻿namespace GenBOE.Web.ModelView
+{
+	using System;
+
+	[Serializable]
+	public class ExportCLINModelView
+	{
+		public ExportCLINModelView() 
+		{
+			this.workspaceShortName = String.Empty;
+		}
+
+		public ExportCLINModelView(string workspaceShortName)
+		{
+			this.workspaceShortName = workspaceShortName;
+		}
+
+		/// <summary>
+		/// workspace shortname
+		/// </summary>
+		public string workspaceShortName { get; set; }
+	}
+}
