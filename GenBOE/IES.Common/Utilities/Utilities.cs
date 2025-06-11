9e01fc6877f4a8efb59cc500d5297f37881c647e
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-06-11 16:02:14 -0400

    Clean up web config

--- GenBOE/IES.Common/Utilities/Utilities.cs
+++ GenBOE/IES.Common/Utilities/Utilities.cs
@@ -1145,6 +1145,12 @@ namespace IES.Common
 		/// <returns>True to show UCOT</returns>
 		public static bool ShowUCOTForWorkspace(DateTime? workspaceCreationDate, string shortname)
 		{
+			// UCOT is space only, no need to run logic if it isn't space.
+			if (SystemConfiguration.Instance().CompanyMode != CompanyConfiguration.SpaceSystems)
+			{
+				return false;
+			}
+
 			// Exclude UCOT showing for specific PTM tracking numbers.
 			string excludedShortspaces = ConfigurationUtilities.GetAppSetting("UcotExcludedWorkspaces");
 			string[] excludedShortspacesArray = excludedShortspaces?.Split(',').Select(s => s.Trim()).ToArray();
