de5c4dde7aeeeb7cca7906d32d166f86b0fd8458
e374897 <e374897@US05CG21630KD>
2025-06-16 14:22:34 -0700

    undo change

--- GenBOE/GenBOE.ModelView/DTO/BOETaskElement/BoeTaskElementDTODataLoader.cs
+++ GenBOE/GenBOE.ModelView/DTO/BOETaskElement/BoeTaskElementDTODataLoader.cs
@@ -1023,7 +1023,7 @@ namespace GenBOE.DataBridge.DTO
 					this.SaveOrdinaryVariables(inTaskDetail.OrdinaryVariables);
 				}
 
-				if (true)
+				if (Utilities.IsSkillMixEnabledForSystem)
 				{
 					// Save the Skill Mix tables
 					if (inTaskDetail.SkillMixTable != null && inTaskDetail.SkillMixTable.Any())
