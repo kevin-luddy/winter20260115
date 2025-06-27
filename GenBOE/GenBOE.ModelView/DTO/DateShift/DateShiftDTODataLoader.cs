da8422e66dd9f935f6eeaa49b8ea23f219de02ef
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-06-27 12:39:18 -0400

    restore loader

--- GenBOE/GenBOE.ModelView/DTO/DateShift/DateShiftDTODataLoader.cs
+++ GenBOE/GenBOE.ModelView/DTO/DateShift/DateShiftDTODataLoader.cs
@@ -316,7 +316,7 @@ namespace GenBOE.DataBridge.DTO
 									StartDate = c.CLINStartDate,
 									EndDate = c.CLINEndDate,
 									UpdateDate = c.UpdateDT,
-									WorkspaceID= c.WorkspaceID,
+									WorkspaceID = c.WorkspaceID,
 								}).FirstOrDefault();
 
 				return new FullClin(clin);
@@ -360,48 +360,8 @@ namespace GenBOE.DataBridge.DTO
 															EndDateValue = lT.BOELaborTypeEndDate,
 															SpreadType = lT.SpreadTypeID.HasValue ? (SpreadType)lT.SpreadTypeID.Value : SpreadType.NotSet,
 															UpdateDate = lT.UpdateDT,
-															SpreadCurveIDValue = lT.SpreadTypeID,
-															LaborTypeOrder = lT.LaborSortId,
-															LaborSpreadsIEnum = lT.BOELaborSpreads
-																.Select(lS => new ResourceSpreadDto
-																{
-																	Id = lS.BOELaborSpreadID,
-																	LaborSpreadDate = lS.LaborSpreadDate,
-																	LaborSpreadValue = lS.LaborSpreadValue ?? 0,
-																	LaborTypeId = lS.BOELaborTypeID
-																}),
-															CustomFieldValueContainersIEnum = lT.BOELaborTypeCustomFieldValueXREFs
-																.Select(cf => new CustomFieldValueContainer
-																{
-																	ContainerID = cf.BLTCFVID,
-																	CustomFieldValueID = cf.CustomFieldValueID,
-																	CustomFieldID = cf.CustomFieldValue.CustomFieldID,
-																	UpdateDate = cf.UpdateDT,
-																	IsOpenEnded = cf.CustomFieldValue.CustomField.IsOpenEnded,
-																	OpenEndedValue = cf.CustomFieldValue.CustomFieldValueDescription
-																})
 														}).ToCollection();
 				}
-
-				boeTaskElement.taskElementLabors.ToList().ForEach(labors =>
-				{
-					labors.CustomFieldValueContainers = labors.CustomFieldValueContainersIEnum.ToCollection(); 
-					labors.CustomFieldValueContainersIEnum = null;
-
-					if (labors.SpreadCurveID == SpreadCurves.DiscreteCost || labors.SpreadCurveID == SpreadCurves.DiscreteHours)
-					{
-						labors.LaborSpreads = labors.LaborSpreadsIEnum.ToCollection();
-					}
-
-					// Always clear out the enumeration
-					labors.LaborSpreadsIEnum = null;
-
-					labors.LaborSpreads.ToList().ForEach(spreads =>
-					{
-						spreads.LaborSpreadDate = spreads.LaborSpreadDate.Normalize();
-						spreads.BoeID = boeTaskElement.BoeID;
-					});
-				});
 
 				return boeTaskElement;
 			}
@@ -419,14 +379,14 @@ namespace GenBOE.DataBridge.DTO
 				gbe.Database.CommandTimeout = 360;  // give queries enough time to execute
 
 				FullWorkspace workspace = (from w in gbe.Workspaces
-										  where w.WorkspaceID == id
-										  select new FullWorkspace
-										  {
-											  Id = w.WorkspaceID,
-											  ContractStartDate = w.ContractStartDate,
-											  ContractEndDate = w.ContractEndDate,
-											  UpdateDate = w.UpdateDT,
-										  }).FirstOrDefault();
+										   where w.WorkspaceID == id
+										   select new FullWorkspace
+										   {
+											   Id = w.WorkspaceID,
+											   ContractStartDate = w.ContractStartDate,
+											   ContractEndDate = w.ContractEndDate,
+											   UpdateDate = w.UpdateDT,
+										   }).FirstOrDefault();
 
 				return workspace;
 			}
