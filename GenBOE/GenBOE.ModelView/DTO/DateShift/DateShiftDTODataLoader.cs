c8974f31c93833c2880fd41b54e55920a12baa1a
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-06-28 00:33:38 -0400

    adjust spread fix

--- GenBOE/GenBOE.ModelView/DTO/DateShift/DateShiftDTODataLoader.cs
+++ GenBOE/GenBOE.ModelView/DTO/DateShift/DateShiftDTODataLoader.cs
@@ -187,25 +187,23 @@ namespace GenBOE.DataBridge.DTO
 		/// </summary>
 		/// <param name="dateShiftable">Date shiftable.</param>
 		/// <returns>List of date shift dtos that were in the child property.</returns>
-		private List<DateShiftDTO> RecursivelyGetDateShiftDTOs(IDateShiftable dateShiftable)
+		private List<DateShiftDTO> RecursivelyGetDateShiftDTOs(DateShiftDTO dateShiftDTO)
 		{
-			if (dateShiftable == null)
+			if (dateShiftDTO == null)
 			{
-				throw new ArgumentNullException(nameof(dateShiftable));
+				throw new ArgumentNullException(nameof(dateShiftDTO));
 			}
 
 			List<DateShiftDTO> dateShiftDTOs = new List<DateShiftDTO>();
-
-			DateShiftDTO dateShiftDTO = DateShiftDTO.FromIDateShiftable(dateShiftable);
 
 			if (dateShiftDTO != null)
 			{
 				dateShiftDTOs.Add(dateShiftDTO);
 			}
 
-			if (dateShiftable.Children != null)
+			if (dateShiftDTO.Children != null)
 			{
-				foreach (IDateShiftable child in dateShiftable.Children)
+				foreach (DateShiftDTO child in dateShiftDTO.Children)
 				{
 					dateShiftDTOs.AddRange(RecursivelyGetDateShiftDTOs(child));
 				}
@@ -316,7 +314,7 @@ namespace GenBOE.DataBridge.DTO
 									StartDate = c.CLINStartDate,
 									EndDate = c.CLINEndDate,
 									UpdateDate = c.UpdateDT,
-									WorkspaceID= c.WorkspaceID,
+									WorkspaceID = c.WorkspaceID,
 								}).FirstOrDefault();
 
 				return new FullClin(clin);
@@ -382,7 +380,7 @@ namespace GenBOE.DataBridge.DTO
 					}
 				}
 
-				
+
 
 				return boeTaskElement;
 			}
@@ -400,14 +398,14 @@ namespace GenBOE.DataBridge.DTO
 				gbe.Database.CommandTimeout = 360;  // give queries enough time to execute
 
 				FullWorkspace workspace = (from w in gbe.Workspaces
-										  where w.WorkspaceID == id
-										  select new FullWorkspace
-										  {
-											  Id = w.WorkspaceID,
-											  ContractStartDate = w.ContractStartDate,
-											  ContractEndDate = w.ContractEndDate,
-											  UpdateDate = w.UpdateDT,
-										  }).FirstOrDefault();
+										   where w.WorkspaceID == id
+										   select new FullWorkspace
+										   {
+											   Id = w.WorkspaceID,
+											   ContractStartDate = w.ContractStartDate,
+											   ContractEndDate = w.ContractEndDate,
+											   UpdateDate = w.UpdateDT,
+										   }).FirstOrDefault();
 
 				return workspace;
 			}
--- GenBOE/GenBOE.ModelView/GenBOE.Dtos/DTOs/DateShiftDTO.cs
+++ GenBOE/GenBOE.ModelView/GenBOE.Dtos/DTOs/DateShiftDTO.cs
@@ -22,7 +22,6 @@ namespace GenBOE.Dtos
 	[Serializable()]
 	public sealed class DateShiftDTO : UpdateableDTO, IDateShiftable
 	{
-		private readonly List<IDateShiftable> children = new List<IDateShiftable>();
 		private bool hasSpread;
 		private object originalObject;
 
@@ -37,9 +36,14 @@ namespace GenBOE.Dtos
 		public DateTime? EndDate { get; set; }
 
 		/// <summary>
+		/// Children date shift objects.
+		/// </summary>
+		public List<IDateShiftable> Children { get; set; } = new List<IDateShiftable>();
+
+		/// <summary>
 		/// Child date shift objects.
 		/// </summary>
-		ICollection<IDateShiftable> IDateShiftable.Children => children;
+		ICollection<IDateShiftable> IDateShiftable.Children => Children;
 
 		/// <summary>
 		/// Parent of the date shift object.
@@ -221,7 +225,7 @@ namespace GenBOE.Dtos
 
 				if (childDTO != null)
 				{
-					dateShiftDTO.children.Add(childDTO);
+					dateShiftDTO.Children.Add(childDTO);
 				}
 			}
 
