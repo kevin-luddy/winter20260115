6826a1933baac11ea2cd2e9254e3dc36236041f7
Yemi <oyeyemi.p.oyetoro@lmco.com>
2025-07-16 17:33:07 -0500

    updated 2025.3 script

--- GenBOE.Database/PTM/Release Scripts/Release 2025.3/1 - Release 2025.3 Script.sql
+++ GenBOE.Database/PTM/Release Scripts/Release 2025.3/1 - Release 2025.3 Script.sql
@@ -3,7 +3,7 @@ GO
 
 -- Author: Oyeyemi Oyetoro (e378233)
 -- JIRA Story: PROPH-2835
--- Added Program Mgr to Role table
+-- Added Program Mgr and MSAC POC to Role table
 
 IF NOT EXISTS (SELECT * FROM dbo.[RoleLU] WHERE RoleID IN (25))
 BEGIN
@@ -13,6 +13,19 @@ BEGIN
 		(RoleID, [Role])
 	VALUES
 		(25, 'Program Mgr')
+		
+	SET IDENTITY_INSERT dbo.[RoleLU] OFF;
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM dbo.[RoleLU] WHERE RoleID IN (26))
+BEGIN
+	SET IDENTITY_INSERT dbo.[RoleLU] ON;
+
+	INSERT INTO [RoleLU]
+		(RoleID, [Role])
+	VALUES
+		(26, 'MSAC POC (Business Development)')
 		
 	SET IDENTITY_INSERT dbo.[RoleLU] OFF;
 END
