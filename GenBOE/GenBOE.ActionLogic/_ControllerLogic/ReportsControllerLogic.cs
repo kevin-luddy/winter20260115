80344c2bc7664767b7798ec079532c4395cfb41e
Wilson, Timothy <timothy.wilson@lmco.com>
2025-07-27 18:07:05 -0400

    Fix ucot validation for word export

--- GenBOE/GenBOE.ActionLogic/_ControllerLogic/ReportsControllerLogic.cs
+++ GenBOE/GenBOE.ActionLogic/_ControllerLogic/ReportsControllerLogic.cs
@@ -19,7 +19,8 @@ namespace GenBOE.ActionLogic.ControllerLogic
     using GenBOE.ActionLogic.IO.Export.BOE;
     using GenBOE.ActionLogic.ModelView;
     using GenBOE.ActionLogic.Reporting;
-    using GenBOE.ActionLogic.WBS.BOE;
+	using GenBOE.ActionLogic.Validation;
+	using GenBOE.ActionLogic.WBS.BOE;
     using GenBOE.ActionLogic.Workspace;
     using GenBOE.DataBridge.DTO;
     using GenBOE.Dtos;
@@ -226,7 +227,31 @@ namespace GenBOE.ActionLogic.ControllerLogic
                     boeExportModelViews = SortAllBOEReport(boeExportModelViews, sortField, sortDirection, viewDataDictionary);
                 }
             }
-        }
+
+			// Validate for UCOT and multiple MOQ tasks - Space only
+			if (Utilities.ShowUCOTForWorkspace(workspace.CreationDate, workspace.TrackingNumber) && SystemConfiguration.Instance().CompanyMode == CompanyConfiguration.SpaceSystems)
+			{
+				IList<MultiMOQTypeResult> multiMoqResults = new List<MultiMOQTypeResult>();
+				IList<string> allTasks = new List<string>();
+
+				foreach (FullBoe boe in workspace.Boes)
+				{
+					multiMoqResults.Add(MultiMOQTypeUtility.DoTasksHaveMultipleMOQTypes(boe, workspace.CreationDate, workspace.Shortname));
+				}
+
+				if (multiMoqResults.Any() && multiMoqResults.Any(x => x.DoMultiMOQTypesExist))
+				{
+					foreach (MultiMOQTypeResult result in multiMoqResults)
+					{
+						allTasks.AddRange(result.Tasks);
+					}
+
+					string commaSeparatedTasks = string.Join(", ", allTasks);
+					string ucotExceptionString = string.Format(ValidationConstants.MULTI_TASK_WITH_MULTI_MOQ_TYPES_UCOT, commaSeparatedTasks);
+					throw new GenValidationException(ucotExceptionString);
+				}
+			}
+		}
 
 		/// <summary>
 		/// Reusable logic for exporting the "All BOEs" report
