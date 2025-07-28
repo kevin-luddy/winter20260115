7eb46a7c9e3b9767ba516c3c3a1d1c95eae2642f
Wilson, Timothy <timothy.wilson@lmco.com>
2025-07-28 12:36:49 -0400

    PR updates

--- GenBOE/GenBOE.ActionLogic/_ControllerLogic/ReportsControllerLogic.cs
+++ GenBOE/GenBOE.ActionLogic/_ControllerLogic/ReportsControllerLogic.cs
@@ -146,6 +146,30 @@ namespace GenBOE.ActionLogic.ControllerLogic
             List<BoeTaskElementDTO> tasks = workspace.TaskElements.ToList();
             boeSummaryGridModelViews = new List<BOESummaryGridModelView>();
 
+			// Validate for UCOT and multiple MOQ tasks - Space only
+			if (Utilities.ShowUCOTForWorkspace(workspace.CreationDate, workspace.TrackingNumber))
+			{
+				IList<MultiMOQTypeResult> multiMoqResults = new List<MultiMOQTypeResult>();
+				IList<string> allTasks = new List<string>();
+
+				foreach (FullBoe boe in workspace.Boes)
+				{
+					multiMoqResults.Add(MultiMOQTypeUtility.DoTasksHaveMultipleMOQTypes(boe, workspace.CreationDate, workspace.Shortname));
+				}
+
+				if (multiMoqResults.Any(x => x.DoMultiMOQTypesExist))
+				{
+					foreach (MultiMOQTypeResult result in multiMoqResults)
+					{
+						allTasks.AddRange(result.Tasks);
+					}
+
+					string commaSeparatedTasks = string.Join(", ", allTasks);
+					string ucotExceptionString = string.Format(ValidationConstants.MULTI_TASK_WITH_MULTI_MOQ_TYPES_UCOT, commaSeparatedTasks);
+					throw new GenValidationException(ucotExceptionString);
+				}
+			}
+
 			#region Override assigned output format template
 
 			// Get template based on workspace preferences
@@ -228,30 +252,6 @@ namespace GenBOE.ActionLogic.ControllerLogic
                     boeExportModelViews = SortAllBOEReport(boeExportModelViews, sortField, sortDirection, viewDataDictionary);
                 }
             }
-
-			// Validate for UCOT and multiple MOQ tasks - Space only
-			if (Utilities.ShowUCOTForWorkspace(workspace.CreationDate, workspace.TrackingNumber) && SystemConfiguration.Instance().CompanyMode == CompanyConfiguration.SpaceSystems)
-			{
-				IList<MultiMOQTypeResult> multiMoqResults = new List<MultiMOQTypeResult>();
-				IList<string> allTasks = new List<string>();
-
-				foreach (FullBoe boe in workspace.Boes)
-				{
-					multiMoqResults.Add(MultiMOQTypeUtility.DoTasksHaveMultipleMOQTypes(boe, workspace.CreationDate, workspace.Shortname));
-				}
-
-				if (multiMoqResults.Any() && multiMoqResults.Any(x => x.DoMultiMOQTypesExist))
-				{
-					foreach (MultiMOQTypeResult result in multiMoqResults)
-					{
-						allTasks.AddRange(result.Tasks);
-					}
-
-					string commaSeparatedTasks = string.Join(", ", allTasks);
-					string ucotExceptionString = string.Format(ValidationConstants.MULTI_TASK_WITH_MULTI_MOQ_TYPES_UCOT, commaSeparatedTasks);
-					throw new GenValidationException(ucotExceptionString);
-				}
-			}
 		}
 
 		/// <summary>
