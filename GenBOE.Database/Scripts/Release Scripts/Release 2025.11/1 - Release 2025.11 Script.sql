7bf15bcf216064e3afd11a08d8a8c25721b8e1b3
Katie <katherine.k.pham@lmco.com>
2025-08-06 11:10:39 -0600

    Renumber release versioning for conflicts

--- GenBOE.Database/Scripts/Release Scripts/Release 2025.11/1 - Release 2025.11 Script.sql
+++ GenBOE.Database/Scripts/Release Scripts/Release 2025.11/1 - Release 2025.11 Script.sql
@@ -1,4 +1,4 @@
-EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2025.10';
+EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2025.11';
 GO
 
 -- 8/5/2025	Katie Pham (e309214),	PROPH-3202 - Skill Mix Disabling via UI
