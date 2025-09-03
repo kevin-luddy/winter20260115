9ce3acf08a5a045a23ad5d0d7488d886e5257e72
e374897 <e374897@US05CG21630KD>
2025-09-03 09:01:00 -0700

    xml comments

--- GenBOE/GenBOE.ModelView/GenBOE.Dtos/ModelView/Reporting/BOEStatusReportGrid.cs
+++ GenBOE/GenBOE.ModelView/GenBOE.Dtos/ModelView/Reporting/BOEStatusReportGrid.cs
@@ -4,29 +4,65 @@
 	using System.Collections.ObjectModel;
 	using System.ComponentModel.DataAnnotations;
 
+	/// <summary>
+	/// BOEStatusReportGrid used for genBOE Angular
+	/// </summary>
 	public class BOEStatusReportGrid
 	{
+		/// <summary>
+		/// Approvers
+		/// </summary>
 		public Collection<string> Approvers { get; set; }
 
+		/// <summary>
+		/// Authors
+		/// </summary>
 		public Collection<string> Authors { get; set; }
 
+		/// <summary>
+		/// BOEID
+		/// </summary>
 		public int BOEID { get; set; }
 
+		/// <summary>
+		/// BOETitle
+		/// </summary>
 		public string BOETitle { get; set; }
 
+		/// <summary>
+		/// CLIN Number
+		/// </summary>
 		public string CLINNumber { get; set; }
 
+		/// <summary>
+		/// CLIN Padded Number
+		/// </summary>
 		public string CLINPaddedNumber { get; set; }
 
+		/// <summary>
+		/// CLIN Title
+		/// </summary>
 		public string CLINTitle { get; set; }
 
+		/// <summary>
+		/// Decimal Precision String Format
+		/// </summary>
 		public string DecimalPrecisionStringFormat { get; set; }
 
+		/// <summary>
+		/// End Date
+		/// </summary>
 		[DisplayFormat(DataFormatString = "{0:MM/yyyy}")]
 		public DateTime EndDate { get; set; }
 
+		/// <summary>
+		/// Is Material
+		/// </summary>
 		public Boolean IsMaterial { get; set; }
 
+		/// <summary>
+		/// Is Multi CLIN WBS
+		/// </summary>
 		public Boolean IsMultiClinWbs { get; set; }
 
 		/// <summary>
@@ -39,13 +75,25 @@
 		/// </summary>
 		public int ResourceDecimalPrecision { get; set; }
 
+		/// <summary>
+		/// Start Date
+		/// </summary>
 		[DisplayFormat(DataFormatString = "{0:MM/yyyy}")]
 		public DateTime StartDate { get; set; }
 
+		/// <summary>
+		/// Status
+		/// </summary>
 		public string Status { get; set; }
 
+		/// <summary>
+		/// Total Cost
+		/// </summary>
 		public decimal TotalCost { get; set; }
 
+		/// <summary>
+		/// Total Hours
+		/// </summary>
 		public decimal TotalHours { get; set; }
 
 		/// <summary>
@@ -73,12 +121,24 @@
 		/// </summary>
 		public string TotalUCOTHoursFormatted { get; set; }
 
+		/// <summary>
+		/// WBS Number
+		/// </summary>
 		public string WBSNumber { get; set; }
 
+		/// <summary>
+		/// WBS Padded Number
+		/// </summary>
 		public string WBSPaddedNumber { get; set; }
 
+		/// <summary>
+		/// WBS Title
+		/// </summary>
 		public string WBSTitle { get; set; }
 
+		/// <summary>
+		/// WorkspaceID
+		/// </summary>
 		public int WorkspaceID { get; set; }
 	}
 }
