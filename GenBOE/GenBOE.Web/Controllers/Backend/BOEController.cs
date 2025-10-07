28ca934866b089008e0ba4a5ddd9e456796c5842
e374897 <e374897@US05CG21630KD>
2025-10-07 15:49:44 -0700

    working endpoint for GetTaskElementGrid

--- GenBOE/GenBOE.Web/Controllers/Backend/BOEController.cs
+++ GenBOE/GenBOE.Web/Controllers/Backend/BOEController.cs
@@ -14,12 +14,15 @@ namespace GenBOE.Web.Controllers.Backend
 	using System.Web.Http.Cors;
 	using GenBOE.ActionLogic;
 	using GenBOE.ActionLogic.Common;
+	using GenBOE.ActionLogic.ModelView;
 	using GenBOE.ActionLogic.ModelView.BOE;
+	using GenBOE.ActionLogic.Validation;
 	using GenBOE.DataBridge.Common.Interfaces;
 	using GenBOE.DataBridge.DTO;
 	using GenBOE.Dtos;
 	using GenBOE.Objects;
 	using IES.Common;
+	using Microsoft.VisualBasic.Logging;
 
 	/// <summary>
 	/// BOEController used for /boe/editboeindex/boe/
@@ -38,6 +41,11 @@ namespace GenBOE.Web.Controllers.Backend
 		private IBOEControllerLogic boeControllerLogic { get; set; }
 
 		/// <summary>
+		/// Task Element Validation
+		/// </summary>
+		private TaskElementValidation taskElementValidation { get; set; }
+
+		/// <summary>
 		/// Ctor
 		/// </summary>
 		/// <param name="securityAccess">Security Access</param>
@@ -46,10 +54,11 @@ namespace GenBOE.Web.Controllers.Backend
 		/// <param name="permissionsLoader">Permission loader</param>
 		/// <param name="homeControllerLogic">Home Controller Logic</param>
 		public BOEController(ISecurityAccess securityAccess, IFullObjectFactory factory, IUserDTODataLoader userLoader, IPermissionsDTODataLoader permissionsLoader,
-			IBOEControllerLogic boeControllerLogic)
+			IBOEControllerLogic boeControllerLogic, TaskElementValidation taskElementValidation)
 			: base(securityAccess, factory, userLoader, permissionsLoader)
 		{
 			this.boeControllerLogic = boeControllerLogic;
+			this.taskElementValidation = taskElementValidation;
 		}
 
 		/// <summary>
@@ -81,6 +90,40 @@ namespace GenBOE.Web.Controllers.Backend
 			}
 
 			FinalizeAction(logger, WebConstants.GET_BOE_HEADER, sw);
+			return result;
+		}
+
+		[HttpGet]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006: Do not nest generic types in member signatures")]
+		public IESSingleResponse<GenericTaskElementGridModelView> GetTaskElementGrid(string workspaceShortname, int boeId)
+		{
+			IESSingleResponse<GenericTaskElementGridModelView> result = new IESSingleResponse<GenericTaskElementGridModelView>();
+
+			FullWorkspace ws = this.Factory.CreateFullWorkspace(workspaceShortname);
+			FullBoe boe = ws.Boes.First(x => x.Id == boeId);
+			Stopwatch sw = InitializeAction(logger, WebConstants.GET_TASK_ELEMENT_GRID, SecurityPage.BOELaborGrid, SecurityAuthorization.Read, new List<WorkspaceDTO> { ws }, boeId);
+
+			try
+			{
+				GenericTaskElementGridModelView theModelView = boeControllerLogic.GetTaskGridModelView(boe, ws);
+				result.Data = theModelView;
+				result.Data.TaskElements = theModelView.TaskElements.OrderBy(teOrder => teOrder.BOETaskElementOrder).ThenBy(teOrder => teOrder.TaskElementDetailID).ToCollection();
+
+				ICollection<int> invalidTaskElementIds = this.taskElementValidation.GetInvalidTaskElementIds(ws, boe.TaskElements);
+				theModelView.TaskElements
+					.AsParallel()
+					.Where(x => x.TaskElementDetailID.HasValue && invalidTaskElementIds.Contains(x.TaskElementDetailID.Value))
+					.ForAll(z => z.FailedValidation = true);
+				result.IsSuccessful = true;
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+				result.Messages.Add(ex.Message);
+			}
+
+			FinalizeAction(logger, WebConstants.GET_TASK_ELEMENT_GRID, sw);
 			return result;
 		}
 	}
