76127ee33b38721ccf2111bb681f2f3cfa3a74e1
e374897 <e374897@US05CG21630KD>
2025-10-19 21:32:00 -0700

    working delete task element

--- GenBOE/GenBOE.Web/Controllers/Backend/BOEController.cs
+++ GenBOE/GenBOE.Web/Controllers/Backend/BOEController.cs
@@ -22,6 +22,7 @@ namespace GenBOE.Web.Controllers.Backend
 	using GenBOE.Dtos;
 	using GenBOE.Objects;
 	using GenBOE.Web.Common;
+	using GenBOE.Web.ModelView;
 	using IES.Common;
 	using Microsoft.VisualBasic.Logging;
 
@@ -148,17 +149,17 @@ namespace GenBOE.Web.Controllers.Backend
 		[HttpDelete]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006: Do not nest generic types in member signatures")]
-		public IESSingleResponse<bool> DeleteTaskElement(string workspaceShortname, int boeId, GenericTaskElementGridRow deletedTask)
+		public IESSingleResponse<bool> DeleteTaskElement([FromBody] DeleteTaskElementModelView deleteTaskElementModelView)
 		{
 			IESSingleResponse<bool> result = new IESSingleResponse<bool>();
 
-			FullWorkspace ws = this.Factory.CreateFullWorkspace(workspaceShortname);
-			FullBoe boe = ws.Boes.First(x => x.Id == boeId);
-			Stopwatch sw = InitializeAction(logger, WebConstants.DELETE_TASK_ELEMENT, SecurityPage.BOELaborGrid, SecurityAuthorization.CreateReadUpdateDelete, new List<WorkspaceDTO> { ws }, boeId);
+			FullWorkspace ws = this.Factory.CreateFullWorkspace(deleteTaskElementModelView.workspaceShortName);
+			FullBoe boe = ws.Boes.First(x => x.Id == deleteTaskElementModelView.boeId);
 
 			try
 			{
-				boeControllerLogic.DeleteTaskElement(ws, boe, deletedTask);
+				Stopwatch sw = InitializeAction(logger, WebConstants.DELETE_TASK_ELEMENT, SecurityPage.BOELaborGrid, SecurityAuthorization.CreateReadUpdateDelete, new List<WorkspaceDTO> { ws }, deleteTaskElementModelView.boeId);
+				boeControllerLogic.DeleteTaskElement(ws, boe, deleteTaskElementModelView.deletedTask);
 				result.IsSuccessful = true;
 				result.Data = true;
 
---
+++ GenBOE/GenBOE.Web/ModelView/DeleteTaskElementModelView.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2025 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+namespace GenBOE.Web.ModelView
+{
+	using System;
+	using GenBOE.ActionLogic.ModelView;
+
+	/// <summary>
+	/// Payload model for HttpDelete DeleteTaskElement
+	/// </summary>
+	[Serializable]
+	public class DeleteTaskElementModelView
+	{
+		public DeleteTaskElementModelView()
+		{
+			this.workspaceShortName = String.Empty;
+			this.boeId = -1;
+			this.deletedTask = null;
+		}
+
+		public DeleteTaskElementModelView(string workspaceShortName, int boeId, GenericTaskElementGridRow deletedTask)
+		{
+			this.workspaceShortName = workspaceShortName;
+			this.boeId = boeId;
+			this.deletedTask = deletedTask;
+		}
+
+		/// <summary>
+		/// Workspace short name
+		/// </summary>
+		public string workspaceShortName { get; set; }
+
+		/// <summary>
+		/// boeId where the Task Element is being deleted from
+		/// </summary>
+		public int boeId { get; set; }
+
+		/// <summary>
+		/// Task Element to be deleted
+		/// </summary>
+		public GenericTaskElementGridRow deletedTask { get; set; }
+	}
+}
