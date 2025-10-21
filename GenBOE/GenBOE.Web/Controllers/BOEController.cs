e4ad599df104852e351170d4126eb95aefc377ad
RJ <ronald.anzalone@lmco.com>
2025-10-21 15:20:35 -0400

    3286 - old UI LM Nav button

--- GenBOE/GenBOE.Web/Controllers/BOEController.cs
+++ GenBOE/GenBOE.Web/Controllers/BOEController.cs
@@ -679,6 +679,7 @@ namespace GenBOE.Web.Controllers
 			ViewData["WSPERFORGS"] = _BoeLaborControllerLogic.GetPerformingOrgs(ws);
 			ViewBag.WsClins = ws.Clins.Where(x => !x.ClinNumber.Equals("MULTI")).Select(x => new { ClinId = x.Id, ClinName = x.ClinString }).ToList();
 			ViewBag.WsWbss = ws.WbsElements.Where(x => !x.WbsNumber.Equals("MULTI")).Select(x => new { WbsId = x.Id, WbsName = x.WbsString }).ToList();
+			ViewData["EnableLmNavigator"] = Utilities.IsLmNavigatorRteLinkEnabledForSystem && ws.EnableLmNavigator;
 
 			// display when you are not a sub, the boe is not summary or multi & ODC items exist
 			ViewData["displayODCTab"] = !(IsSubContractor || boe.IsMultiClinWbs) && boe.OtherDirectCosts.Any();
