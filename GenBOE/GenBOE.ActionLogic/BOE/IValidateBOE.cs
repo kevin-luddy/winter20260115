05ea9ff90183edc8335ccba6068ac15e87c7b437
e374897 <e374897@US05CG21630KD>
2025-10-28 14:58:14 -0700

    whitespace fix

--- GenBOE/GenBOE.ActionLogic/BOE/IValidateBOE.cs
+++ GenBOE/GenBOE.ActionLogic/BOE/IValidateBOE.cs
@@ -15,22 +15,22 @@ namespace GenBOE.ActionLogic.WBS.BOE
 	/// IValidate Interface
 	/// </summary>
 	public interface IValidateBOE
-    {
-        /// <summary>
-        /// Validate all BOE data (BOE Header, task element details, labor type, and labor spreads)
-        /// when the user selects the Validate button
-        /// </summary>
-        /// <param name="inBOE">the BOE DTO to validate</param>
-        /// <param name="ws">Full WS</param>
-        /// <returns>all possible validation messages</returns>
-        ValidationBOEModelView ValidateBOE_OnValidateBtnClick(FullBoe inBOE, FullWorkspace ws);
+	{
+		/// <summary>
+		/// Validate all BOE data (BOE Header, task element details, labor type, and labor spreads)
+		/// when the user selects the Validate button
+		/// </summary>
+		/// <param name="inBOE">the BOE DTO to validate</param>
+		/// <param name="ws">Full WS</param>
+		/// <returns>all possible validation messages</returns>
+		ValidationBOEModelView ValidateBOE_OnValidateBtnClick(FullBoe inBOE, FullWorkspace ws);
 
-        /// <summary>
-        /// Validate all BOE data (BOE Header, task element details, labor type, and labor spreads) in the workspace
-        /// </summary>
-        /// <param name="ws">Full Ws</param>
-        /// <returns>Validation data</returns>
-        ValidationAllBOEModelView ValidateAllBOEs(FullWorkspace ws);
+		/// <summary>
+		/// Validate all BOE data (BOE Header, task element details, labor type, and labor spreads) in the workspace
+		/// </summary>
+		/// <param name="ws">Full Ws</param>
+		/// <returns>Validation data</returns>
+		ValidationAllBOEModelView ValidateAllBOEs(FullWorkspace ws);
 
 		/// <summary>
 		/// Validate MOQ Template data on a Task Level. Does NOT validate Labor Type level selection
