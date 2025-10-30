0fb80d57153f23a1341c6e7e4b13ec5b59208635
Hazrat Rafiqzadah <hazrat.b.rafiqzadah@lmco.com>
2025-10-30 17:53:45 -0400

    Fixed typo

--- GenBOE.Database/PTM/Release Scripts/Release 2025.8/1 - Release 2025.8 Script.sql
+++ GenBOE.Database/PTM/Release Scripts/Release 2025.8/1 - Release 2025.8 Script.sql
@@ -2,7 +2,7 @@ EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2025.8';
 GO
 
 /******************************************************************************
-** This only servers the purpose of allowing the full release script to generated
+** This only serves the purpose of allowing the full release script to be generated
 ** with required changes for Stored Procedures and Views
 ** 10/30/25		e403038				PROPH-3406 Label Modifications => Current Planned renamed to ScheduledActual and Current Scheduled renamed to Planned
 
