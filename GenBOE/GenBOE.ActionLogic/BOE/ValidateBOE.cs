dfb557c4584daaab7b4f21e26cf5d2a401c29862
Wilson, Timothy <timothy.wilson@lmco.com>
2025-11-13 11:53:23 -0500

    Reset to 0

--- GenBOE/GenBOE.ActionLogic/BOE/ValidateBOE.cs
+++ GenBOE/GenBOE.ActionLogic/BOE/ValidateBOE.cs
@@ -247,6 +247,7 @@ namespace GenBOE.ActionLogic.WBS.BOE
 					ICollection<string> errorMessages = new List<string>();
 
 					// Set the task's MOQTotalRelevantHours
+					task.MOQTotalRelevantHours = 0;
 					ICollection<MoqTypeSelection> moqTypes = ws.MoqTypeSelections.Where(m => m.TaskId == task.Id).ToList();
 					// Space will get the total moq total relevant hours if it is from a Sap Webi moq table data.
 					if (SystemConfiguration.Instance().CompanyMode == IES.Common.CompanyConfiguration.SpaceSystems)
