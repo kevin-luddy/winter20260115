7d8edf9dadd032ce4cfc834a59013990abe43c43
Thomas Asuncion <thomas.asuncion@lockheed-martin.com>
2025-12-03 10:36:36 -0500

    Bring changes into minion

--- GenBOE/GenBOE.ActionLogic/Misc/SkillMixUtility.cs
+++ GenBOE/GenBOE.ActionLogic/Misc/SkillMixUtility.cs
@@ -74,7 +74,10 @@
 			refreshedModel.SkillMixRows = refreshedModel.SkillMixRows.OrderBy(r => string.IsNullOrWhiteSpace(r.ResourceOld)).ThenBy(r => r.ResourceOld).ToList();
 			refreshedModel.CommonDisclosureRows = refreshedModel.CommonDisclosureRows.OrderBy(r => string.IsNullOrWhiteSpace(r.ResourceID)).ThenBy(r => r.ResourceID).ThenBy(s => s.BusinessResourceID).ToList();
 			refreshedModel.SkillMixSummaryRows = refreshedModel.SkillMixSummaryRows.OrderBy(r => string.IsNullOrWhiteSpace(r.ResourceID)).ThenBy(r => r.ResourceID).ThenBy(s => s.BusinessResourceID).ToList();
-			
+
+			// Apply rationale canned responses based on conditions regarding proposed skill mix and historical hours.
+			ApplyRationaleCannedResponses(refreshedModel);
+
 			return refreshedModel;
 		}
 
@@ -742,6 +745,30 @@
 			refreshedModel.SkillMixTotals.ProposedSkillMix = refreshedModel.SkillMixRows.Where(d => d.Included).Sum(s => s.BOESkillMix ?? 0.0m);
 			refreshedModel.CommonDisclosureTotals.ProposedSkillMix = refreshedModel.CommonDisclosureRows.Where(d => d.Included).Sum(s => s.BOESkillMix ?? 0.0m);
 			refreshedModel.SkillMixSummaryTotals.ProposedSkillMix = refreshedModel.SkillMixSummaryRows.Sum(s => s.ProposedSkillMix ?? 0.0m);
+		}
+
+		/// <summary>
+		/// Apply rationale canned responses based on proposed skill mix, proposed hours and historial hours
+		/// </summary>
+		/// <param name="refreshedModel">Skill mix data.</param>
+		private static void ApplyRationaleCannedResponses(RefreshSkillMixModelView refreshedModel)
+		{
+			foreach (SkillMixSummaryModelView row in refreshedModel.SkillMixSummaryRows)
+			{
+				// Get the difference between Proposed Skill Mix and Historical Skill Mix.
+				decimal difference = Math.Abs(row.ProposedSkillMix.Value - row.HistoricalSkillMix);
+
+				// Check if the difference is less than 5%.
+				if (difference < 5m)
+				{
+					row.Rationale = Constants.SPACE_SKILL_MIX_RATIONALE_BELOW_5_PERCENT_DIFF;
+					row.IsRationaleReadOnly = true;
+					continue;
+				}
+
+				// If neither conditions are met then leave it as is.
+				row.IsRationaleReadOnly = false;
+			}
 		}
 
 		/// <summary>
