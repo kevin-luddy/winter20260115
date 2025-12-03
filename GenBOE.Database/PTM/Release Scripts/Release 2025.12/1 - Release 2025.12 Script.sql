71d14814e8c108b9bafd4607abf7a4c89d86aae3
Yansen Tjandra <yansen.tjandra@lmco.com>
2025-12-03 16:25:43 -0500

    Resolve comments

--- GenBOE.Database/PTM/Release Scripts/Release 2025.12/1 - Release 2025.12 Script.sql
+++ GenBOE.Database/PTM/Release Scripts/Release 2025.12/1 - Release 2025.12 Script.sql
@@ -1,4 +1,4 @@
-EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2025.11';
+EXEC [dbo].[UpdateDbVersion] @DbVersion = '1', @AppVersion = '2025.12';
 GO
 
 -- Author: Yansen Tjandra (e402751)
