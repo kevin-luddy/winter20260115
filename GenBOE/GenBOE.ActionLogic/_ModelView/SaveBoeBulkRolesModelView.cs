27065d207ba4ac34a5ba44d5ac8e23d8d2113b0f
Yemi <oyeyemi.p.oyetoro@lmco.com>
2026-01-13 09:34:01 -0600

    added SaveBoeBulkRoles api controller method

---
+++ GenBOE/GenBOE.ActionLogic/_ModelView/SaveBoeBulkRolesModelView.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright company="Lockheed Martin Corporation">
+//     Copyright (c) 2011 - 2026 Lockheed Martin Corporation
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GenBOE.ActionLogic._ModelView
+{
+	using GenBOE.ActionLogic.ModelView;
+	using System.Collections.Generic;
+
+	public class SaveBoeBulkRolesModelView
+	{
+		public SaveBoeBulkRolesModelView()
+		{
+			this.Workspace = string.Empty;
+			this.BoeRolesToSave = new List<ManageBOEModelView>();
+		}
+
+		public string Workspace { get; set; }
+
+		public ICollection<ManageBOEModelView> BoeRolesToSave { get; set; }
+	}
+}
